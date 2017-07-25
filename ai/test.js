34339f1f970272398da8fcafb7f1be8153d91796
shikong <a26214311@gmail.com>
2017-07-25 12:56:01 +0800

    f

--- ai/test.js
+++
@@ -1,7 +0,0 @@
-var ts = new Date().getTime();
-var type = Math.floor(Math.random()*8)+1;
-var id = (1999123456789-ts-86400000*type)+"_"+type;
-var country = "86";
-var phone = country+"123456"+(Math.floor(Math.random()*88888)+11111);
-var save = {"_id":id,type:NumberInt(type),c:country,phone:phone,ts:new Date(ts)}
-db.cl_winner.save(save)
