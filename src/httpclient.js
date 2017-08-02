d28d0ee49352754837b4e7a6ed3e1526bc543fda
shikong <a26214311@gmail.com>
2017-08-02 10:52:28 +0800

    https

--- src/httpclient.js
+++ src/httpclient.js
@@ -30,7 +30,7 @@ class HttpClient {
     get clientHeaders() {
         return {
             Cookie: this.getCookieString(),
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0'
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'
         };
     }
 
