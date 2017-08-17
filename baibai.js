bab288efcf404374d6bfca3a66868b3cc1074ebf
shikong <a26214311@gmail.com>
2017-08-17 12:32:23 +0800

    fix

--- baibai.js
+++ baibai.js
@@ -91,7 +91,7 @@ function handleMsg_D(msg,qq,type){
     var c1 = content.substring(1);
     if(c1==""){
       var ret = "`1+名词：百科查询\n翻译成中文：`+要翻译的内容\n翻译成日文：`2+要翻译的内容\n翻译成英文：`3+要翻译的内容\n";
-      ret = ret + "`4+内容：百度查询\n`5汇率转换\n`0+数字：大写数字转换\n`8+地点A-地点B：公交查询\n";
+      ret = ret + "`4+内容：百度查询\n`c汇率转换\n`0+数字：大写数字转换\n`8+地点A-地点B：公交查询\n";
       ret = ret + "天气预报：城市名+天气\n教百百说话：问题|答案\n计算器：直接输入算式\n虾扯蛋：``+对话";
       callback(ret);
     }else{
@@ -155,7 +155,7 @@ function reply(content,userName,callback){
     baiduSearch(userName,content.substring(1),callback);
   }else if(first==0){
     callback(money(content.substring(1)));
-  }else if(first==5){
+  }else if(first=='c'||first=='C'){
     callback(xchange(userName,content.substring(1),callback));
   }else if(first==8){
     var ca = content.substring(1).split('-');
