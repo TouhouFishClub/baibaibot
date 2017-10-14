e2fbe14492978c252ca7260b499797019d00d963
a26214311 <a26214311@gmail.com>
2017-10-14 12:17:28 +0800

    Update senka.js

--- ai/kancolle/senka.js
+++ ai/kancolle/senka.js
@@ -397,9 +397,8 @@ function generateTable(sorttype,server){
       var h = '';
       h = h + '排名：'+(i + 1) + '位(' + senka.lno + '位)\n';
       h = h + senka.name+'\n';
-      h = h + '战果值：'+senka.senka + '\n';
-      h = h + '最大值：'+max + '\n';
-      h = h + '最小值：'+min + '\n';
+      h = h + '当前战果：'+senka.senka + '\n';
+      h = h + '战果(ex)：'+min + '-'+max+'\n';
       h = h + '经验：'+subsenkastr + '\n';
       h = h + 'ex:'+exstr+'\n';
       c[server].u[senka.name]=h;
