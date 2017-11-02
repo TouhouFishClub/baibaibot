b4b0cdd86a43dff4d2dfbd0cedb7494aeceb904a
ter <a26214311@gmail.com>
2017-11-02 09:56:21 +0800

    f

--- ai/banuser.js
+++ ai/banuser.js
@@ -3,7 +3,7 @@ var Axios = require('axios');
 function banuser(content,userName,callback) {
   var time;
   if(content==""){
-    time=Math.floor(Math.random()*600);
+    time=Math.floor(Math.random()*6000);
   }else{
     time = parseInt(content);
   }
@@ -11,7 +11,9 @@ function banuser(content,userName,callback) {
     timeout: 6000,
     headers: {}
   }).then(function(response){
-    console.log('kh');
+    setTimeout(function(){
+      Axios.get('http://54.223.156.168:23334/set_group_ban?group_id=205700800&user_id=705886109&duration=0');
+    },time)
   }).catch(error => {
     console.log(error)
   })
