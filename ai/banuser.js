27367d241be9144d16ff4351119a17ae700b65d1
ter <a26214311@gmail.com>
2017-11-03 10:47:25 +0800

    f

--- ai/banuser.js
+++ ai/banuser.js
@@ -5,9 +5,12 @@ const surl = 'http://localhost:23334/';
 function banuser(content,userName,callback) {
   var time;
   if(content==""){
-    time=Math.floor(Math.random()*6000);
+    time=Math.floor(Math.random()*3600);
   }else{
     time = parseInt(content);
+    if(!time){
+      time=Math.floor(Math.random()*3600);
+    }
   }
   Axios.get(surl+'set_group_ban?group_id=205700800&user_id=705886109&duration='+time, {
     timeout: 6000,
