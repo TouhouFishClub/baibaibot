5f0fcd708b931c838e41835042da83bb1b64ee55
ter <a26214311@gmail.com>
2017-11-03 10:50:33 +0800

    f

--- ai/banuser.js
+++ ai/banuser.js
@@ -12,16 +12,10 @@ function banuser(content,userName,callback) {
       time=Math.floor(Math.random()*3600);
     }
   }
-  Axios.get(surl+'set_group_ban?group_id=205700800&user_id=705886109&duration='+time, {
-    timeout: 6000,
-    headers: {}
-  }).then(function(response){
-    setTimeout(function(){
-      Axios.get(surl+'set_group_ban?group_id=205700800&user_id=705886109&duration=0');
-    },time)
-  }).catch(error => {
-    console.log(error)
-  })
+  banUserbyName(userName,time);
+  setTimeout(function(){
+    banUserbyName(userName,0);
+  },time+500);
 }
 
 var qmap={};
