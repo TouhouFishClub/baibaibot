f06c82cdacb5625d60e0b0800c8086f8fdb9aa1c
ter <a26214311@gmail.com>
2017-11-16 11:55:50 +0800

    f

--- ai/Roulette.js
+++ ai/Roulette.js
@@ -21,11 +21,15 @@ module.exports = function(nickname, content, callback){
       rouletteObj.gamers = []
       rouletteTimer = setTimeout(() => {checkRouletteGammers()}, 60000)
       callback('生死有命，富贵在天！\n俄罗斯轮盘将在 60 秒后开始。\n参加：加入/参加/join\n开枪：开枪/开火/fire')
+    }else{
+      callback('请稍后再试');
     }
   }
   if(rouletteObj.gameStart && !rouletteObj.gameAction &&
     (content === '加入' || content === '加入' || content === 'join' || content === '參加' || content === '参加')
   ){
+
+
     var can=true
     if(death[nickname]) {
       var now = new Date().getTime();
