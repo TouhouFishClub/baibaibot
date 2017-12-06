b0060a91af58fe2391216cf6544b15f9a3c70fd1
ter <a26214311@gmail.com>
2017-12-06 17:49:19 +0800

    f

--- ai/xchange.js
+++ ai/xchange.js
@@ -156,7 +156,7 @@ const formatData = async (codeArr, money, callback) => {
 
 const getOkcoinData = code =>
   new Promise((resolve, reject) => {
-    Axios.get(`https://www.okex.com/api/v1/future_ticker.do?symbol=${code}&contract_type=this_week`, {
+    Axios.get(`https://www.okex.com/api/v1/ticker.do?symbol=${code}`, {
       timeout: TIME_OUT,
       headers: {
         'User-Agent': USER_AGENT
