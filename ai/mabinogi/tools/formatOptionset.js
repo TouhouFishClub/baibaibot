0a67212cec76bc43186871075883651c01623bc9
flandre scarlet <flantan233@gmail.com>
2018-04-09 18:16:43 +0800

    update optionset

--- ai/mabinogi/tools/formatOptionset.js
+++ ai/mabinogi/tools/formatOptionset.js
@@ -1,23 +1,21 @@
 const fs = require('fs')
 const xml2js = require('xml2js')
-module.exports = function() {
+module.exports = function(callback) {
   let optionsetInfo = fs.readFileSync('./optionset_R291', 'utf-8')
   let optionsetXml = fs.readFileSync('./optionset_R291.xml', 'utf-8')
   let parser = new xml2js.Parser()
-  // parser.parseString(optionsetXml, (err, result) => {
-  //   console.log(result)
-  //   console.log('Done.')
-  // })
-  // var xml = "<root>Hello xml2js!</root>"
-  // parser.parseString(xml, (err, result) => {
-  //   console.log(JSON.stringify(result))
-  // })
-  // console.log(optionsetXml)
-  fs.readFile('./optionset_R291.xml', 'utf-8', (err, data) => {
-    console.log(data)
-    parser.parseString(data, (err, result) => {
-      console.log(JSON.stringify(result))
-    })
+  let _LT = {}
+  optionsetInfo.split('\n').forEach(val => {
+    let sp = val.split('\t')
+    _LT[`xml.optionset.${sp[0]}`] = sp[1]
   })
-
+  parser.parseString(optionsetXml, (err, result) => {
+    let options = result.OptionSet.OptionSetList[0].OptionSet.map(val => val.$)
+    options.forEach(val => {
+      val.LocalName = eval(val.LocalName)
+      val.LocalName2 = eval(val.LocalName2)
+      val.OptionDesc = eval(val.OptionDesc)
+    })
+    callback(options)
+  })
 }
--- ai/mabinogi/tools/test.js
+++ ai/mabinogi/tools/test.js
@@ -1,2 +1,12 @@
 const fo = require('./formatOptionset')
-fo()
+const app = require('express')()
+app.listen('8233', () => {
+  console.log('server started')
+  console.log('http://localhost:8233')
+})
+app.get('/', (req, res) => {
+  fo(d => {
+    // console.log(d)
+    res.send(d);
+  })
+})
