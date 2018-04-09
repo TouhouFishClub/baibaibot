cb8ff50f1efddd0a5fcfac00057cdd6eadd5e85e
flandre scarlet <flantan233@gmail.com>
2018-04-09 16:40:58 +0800

    update optionset

--- ai/mabinogi/tools/formatOptionset.js
+++ ai/mabinogi/tools/formatOptionset.js
@@ -2,15 +2,21 @@ const fs = require('fs')
 const xml2js = require('xml2js')
 module.exports = function() {
   let optionsetInfo = fs.readFileSync('./optionset_R291', 'utf-8')
-  let optionsetXml = fs.readFileSync('./optionset.xml', 'utf-8')
+  let optionsetXml = fs.readFileSync('./optionset_R291.xml', 'utf-8')
   let parser = new xml2js.Parser()
-  parser.parseString(optionsetXml, (err, result) => {
-    console.log(result)
-    console.log('Done.')
-  })
-  fs.readFile('./optionset.xml', 'utf-8', (err, data) => {
+  // parser.parseString(optionsetXml, (err, result) => {
+  //   console.log(result)
+  //   console.log('Done.')
+  // })
+  // var xml = "<root>Hello xml2js!</root>"
+  // parser.parseString(xml, (err, result) => {
+  //   console.log(JSON.stringify(result))
+  // })
+  // console.log(optionsetXml)
+  fs.readFile('./optionset_R291.xml', 'utf-8', (err, data) => {
+    console.log(data)
     parser.parseString(data, (err, result) => {
-      console.log(result)
+      console.log(JSON.stringify(result))
     })
   })
 
