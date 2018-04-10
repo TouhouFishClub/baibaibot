581f8fc90d01268f1976325d46713cb12ef190b7
flandre scarlet <flantan233@gmail.com>
2018-04-10 11:24:33 +0800

    update

--- ai/mabinogi/tools/formatOptionset.js
+++ ai/mabinogi/tools/formatOptionset.js
@@ -1,20 +1,22 @@
+import {level} from "../../../index";
+
 const fs = require('fs')
 const xml2js = require('xml2js')
 module.exports = function(callback) {
   let optionsetInfo = fs.readFileSync('./optionset_R291', 'utf-8')
   let optionsetXml = fs.readFileSync('./optionset_R291.xml', 'utf-8')
   let parser = new xml2js.Parser()
-  let _LT = {}
+  let transform = {}
   optionsetInfo.split('\n').forEach(val => {
     let sp = val.split('\t')
-    _LT[`xml.optionset.${sp[0]}`] = sp[1]
+    transform[`_LT[xml.optionset.${sp[0]}]`] = sp[1]
   })
   parser.parseString(optionsetXml, (err, result) => {
     let options = result.OptionSet.OptionSetList[0].OptionSet.map(val => val.$)
     options.forEach(val => {
-      val.LocalName = eval(val.LocalName)
-      val.LocalName2 = eval(val.LocalName2)
-      val.OptionDesc = eval(val.OptionDesc)
+      val.LocalName = transform[val.LocalName]
+      val.LocalName2 = transform[val.LocalName2]
+      val.OptionDesc = transform[val.OptionDesc]
     })
     callback(options)
   })
