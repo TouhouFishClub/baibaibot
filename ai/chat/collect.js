80d77753af5e9e5d3a22091a9b6ed5bd5c73532a
ter <a26214311@gmail.com>
2018-05-04 11:33:58 +0800

    f

--- ai/chat/collect.js
+++ ai/chat/collect.js
@@ -4,7 +4,7 @@ var mongourl = 'mongodb://192.168.17.52:27050/db_bot';
 
 function saveChat(gid,uid,name,content){
   var now = new Date();
-  var data = {'_id':now.getTime(),gid:gid,uid:uid,n:name,d:content};
+  var data = {'_id':now,gid:gid,uid:uid,n:name,d:content,ts:now.getTime()};
   MongoClient.connect(mongourl, function(err, db) {
     var cl_chat = db.collection('cl_chat');
     cl_chat.save(data);
