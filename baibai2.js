055eaa72c78dfa33795677362509663d3f47ac39
flandre scarlet <flantan233@gmail.com>
2018-05-22 10:17:35 +0800

    test

--- baibai2.js
+++ baibai2.js
@@ -262,7 +262,7 @@ function handleMsg_D(msgObj,response){
     rua(callback)
     return
   }
-
+//aaaaa
   let fie = con.substring(0,3)
   if(fie == 'opt' && fi != 'opts'){
     op(name, con.substring(3).trim(), 'image', callback);
