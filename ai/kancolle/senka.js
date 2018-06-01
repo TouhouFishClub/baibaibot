76266893119d06985aac5dbfdbf547c313e450be
ter <a26214311@gmail.com>
2018-06-01 15:14:39 +0800

    f

--- ai/kancolle/senka.js
+++ ai/kancolle/senka.js
@@ -31,9 +31,7 @@ function load(){
 }
 
 var timer = 0;
-collectSenka(8,function(){
-  console.log('end111111111');
-});
+loopsenka();
 function tm(){
   var left = (-new Date().getTime()+25200000)%43200000+43200000
   console.log('will start senka after '+ (left/60000).toFixed(0) + 'minutes')
