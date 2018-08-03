67ecc269c22400f2d7e6364e0189125d8ef121eb
Flandre <flantan233@gmail.com>
2018-08-03 16:37:47 +0800

    fix jrrp

--- ai/rp.js
+++ ai/rp.js
@@ -1,8 +1,28 @@
 const md5 = require("md5")
 module.exports = function(qq, callback){
   let str = `${qq}${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`
-  let rp = parseInt(md5(str).substring(0, 15), 16).toString().split('').reduce((p, c) => p + parseInt(c), 0)
-  if(rp > 100)
-    rp = 100
+  let md = md5(str)
+  let rp = parseInt(md.substring(0, 15), 16).toString().split('').reduce((p, c) => p + parseInt(c), 0)
+  let rpFixType = parseInt(md.substring(15, 16), 16) % 3
+  let rpFix = parseInt(md.substring(16, 20), 16).toString().split('').reduce((p, c) => p + parseInt(c), 0)
+  switch (rpFixType){
+    case 0:
+      rp += rpFix
+      break
+    case 1:
+      rp -= rpFix
+      break
+  }
+  if(rp < 0){
+    rp = Math.abs(rp)
+  }
+  if(rp > 100){
+    if(rp > 120){
+      rp = 100 - (rp - 120)
+    }
+    else {
+      rp = 100
+    }
+  }
   callback(`[CQ:at,qq=${qq}] 今天的运势指数是 ${rp}% ！\n${new Array(rp).fill('|').join('')}`)
 }
