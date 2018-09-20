96de13870c394e71351689ebc70350bb2e310578
ter <a26214311@gmail.com>
2018-09-20 13:59:40 +0800

    fix

--- ai/ff14/item.js
+++ ai/ff14/item.js
@@ -364,21 +364,9 @@ function getItemDetail(itemname,text,itemid,userName,callback,detailresdata){
         ch = '<table class="wikitable item-craft-table filter-div--item">'+ch+'</table>';
       }
 
-      hs = '<div class="mw-parser-output"><div class="table-responsive">'+hs+cs+'</div></div>';
+      hs = '<div class="mw-parser-output"><div class="table-responsive">'+hs+cs+ch+'</div></div>';
       var itemhtml = '';
-      if(ch!=''){
-        console.log('hs:'+hs)
-        ch = '<div class="mw-parser-output"><div class="table-responsive">'+ch+'</div></div>';
-        if(hs.length>200){
-          var uhs = '<table><tr><td>'+text+'</td><td rowspan="2">'+ch+'</td></tr>';
-          uhs = uhs + '<tr><td>'+hs+'</td></tr>';
-          itemhtml = ita[0]+'<div class="ffiv-container" style="float:left">'+uhs+'</div>'+ita[1];
-        }else{
-          itemhtml = ita[0]+'<div class="ffiv-container" style="float:left">'+text+hs+ch+'</div>'+ita[1];
-        }
-      }else{
-        itemhtml = ita[0]+'<div class="ffiv-container" style="float:left">'+text+hs+'</div>'+ita[1];
-      }
+      itemhtml = ita[0]+'<div class="ffiv-container" style="float:left">'+text+hs+'</div>'+ita[1];
       var path = 'ff14/item/'+itemid+'.html'
       fs.writeFileSync('public/'+path,itemhtml);
       getPic(path,nocache?"nocache/"+itemid:itemid,callback)
