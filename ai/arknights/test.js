057a2d0a85fc000e77dcefe553b5d2152c9e9ed7
flandre <flantan233@gmail.com>
2019-05-24 09:16:29 +0800

    ignore normal tag group

--- ai/arknights/test.js
+++ ai/arknights/test.js
@@ -9,11 +9,11 @@ const c = m => {
 }
 // console.log(akk('skchr_plosis_2', '3'))
 // akc('123456', 'c 6 1 2-99   ', c)
-aks('123456', '白面鸮 adfa', c)
+// aks('123456', '白面鸮 adfa', c)
 // aks('123456', '蓝毒 8', c)
 // aks('123456', '蓝毒 9', c)
 // aks('123456', '蓝毒 10', c)
-// ak(123,'[CQ:image,file=EA5A2871D9C3B3095298B7A2EAF96812.jpg,url=https://gchat.qpic.cn/gchatpic_new/799018865/549823679-2449259008-EA5A2871D9C3B3095298B7A2EAF96812/0?vuin=2375373419&term=2]', c)
+ak(123,'[CQ:image,file=AC3933741FBC22E8A2875F536EEC94F5.jpg,url=https://gchat.qpic.cn/gchatpic_new/357474405/2195700800-2744645707-AC3933741FBC22E8A2875F536EEC94F5/0?vuin=2375373419&term=2]', c)
 // ak(123132, '重装 重装 先锋干员 群攻 位移', c)
 // ak('[CQ:image,file=9A38768CEBF470A313C6A4B7441B9242.png,url=https://gchat.qpic.cn/gchatpic_new/912008974/549823679-3005667254-9A38768CEBF470A313C6A4B7441B9242/0?vuin=2375373419&term=2]', c)
 // ak('[CQ:image,file=200EB4D8FBB1A7B34FA47E4240C895BA.png,url=https://gchat.qpic.cn/gchatpic_new/771210053/549823679-2274281053-200EB4D8FBB1A7B34FA47E4240C895BA/0?vuin=2375373419&term=2][CQ:image,file=200EB4D8FBB1A7B34FA47E4240C895BA.png,url=https://gchat.qpic.cn/gchatpic_new/771210053/549823679-2274281053-200EB4D8FBB1A7B34FA47E4240C895BA/0?vuin=2375373419&term=2]', c)
