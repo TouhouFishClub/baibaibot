840253b3beccde7cca6b00bf5d9f344f9c80f20d
flandre <flantan233@gmail.com>
2019-05-29 09:05:28 +0800

    update character

--- ai/arknights/test.js
+++ ai/arknights/test.js
@@ -12,7 +12,7 @@ const c = m => {
 // aks('123456', '白面鸮 adfa', c)
 // aks('123456', '艾雅法拉 8', c)
 // aks('123456', '蓝毒 9', c)
-// aks('123456', '蓝毒 10', c)
+aks('123456', '空爆 10', c)
 // ak(123,'[CQ:image,file=D92B8C3ECBA963C3563701F900F72FF1.jpg,url=https://gchat.qpic.cn/gchatpic_new/799018865/577587780-2346967126-D92B8C3ECBA963C3563701F900F72FF1/0?vuin=2375373419&term=2]', c)
 // ak(123132, '重装 重装 先锋干员 群攻 位移', c)
 // ak('[CQ:image,file=9A38768CEBF470A313C6A4B7441B9242.png,url=https://gchat.qpic.cn/gchatpic_new/912008974/549823679-3005667254-9A38768CEBF470A313C6A4B7441B9242/0?vuin=2375373419&term=2]', c)
