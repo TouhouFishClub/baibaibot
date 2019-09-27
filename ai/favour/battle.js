e7cc29717e71a3a77d3ef36036687900f7adb07c
ter <a26214311@gmail.com>
2019-09-27 10:22:20 +0800

    f

--- ai/favour/battle.js
+++ ai/favour/battle.js
@@ -109,7 +109,7 @@ function sendFightImage(wd,callback){
 
   var img1 = new imageMagick("static/blank.png");
   console.log("len:"+maxwd+":"+len);
-  img1.resize(maxwd*19+29, len*21+29,'!') //加('!')强行把图片缩放成对应尺寸150*150！
+  img1.resize(maxwd*19+29, len*24+29,'!') //加('!')强行把图片缩放成对应尺寸150*150！
     .autoOrient()
     .fontSize(20)
     .fill('blue')
