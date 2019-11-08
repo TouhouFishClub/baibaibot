e35cc049ddbfdcb300eb856f35fe48b13cc7303a
flandre <flantan233@gmail.com>
2019-11-08 17:37:12 +0800

    update

---
+++ ai/ff14/cactpot.js
@@ -0,0 +1,9 @@
+module.exports = function
+
+const PerfectCactpot = (content, callback) => {
+  let c = content.trim().toLowerCase().replace(/[\n ]/g, '').split('')
+  if(c.length == 9 && Array.from(new Set(c)).length == 5) {
+
+  }
+
+}
