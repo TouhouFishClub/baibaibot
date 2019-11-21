a73379d5c04ff4d79219f9136892064924eb3e8c
ter <a26214311@gmail.com>
2019-11-21 12:40:58 +0800

    f

--- ai/ff14/item.js
+++ ai/ff14/item.js
@@ -414,11 +414,11 @@ function getAbsuloteItemDetail(itemname,userName,callback){
       resdata = resdata + chunk;
     });
     res.on('end', function () {
-      var str = 'name="pageId" value="';
+      var str = '//www.garlandtools.org/db/#item/';
       var n1 = resdata.indexOf(str);
       if(n1>0){
         var s1 = resdata.substring(n1+str.length);
-        var n2 = s1.indexOf('"');
+        var n2 = s1.indexOf('/');
         var itemid = s1.substring(0,n2).trim();
         searchFF14ItemByID(itemid,userName,callback,resdata);
       }else{
