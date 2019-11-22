d851fe978336f4ae6701e68f9ba2e399790486bb
Flandre <flantan233@gmail.com>
2019-11-23 02:17:33 +0800

    update avatar

--- ai/ff14/test.js
+++ ai/ff14/test.js
@@ -1,11 +1,13 @@
 const { PerfectCactpot } = require('./cactpot')
+const { fflogs2Reply } = require('./fflogs2')
 const c = m => {
   console.log('===== output =====')
   console.log(m)
 }
 
-PerfectCactpot(
-`a
-8xx
-49x
-7xx`, c)
+// PerfectCactpot(
+// `a
+// 8xx
+// 49x
+// 7xx`, c)
+fflogs2Reply('肥宅 黑魔', '111', c, 1)
