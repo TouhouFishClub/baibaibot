4c40da0dba70a2fc4ee3a53d3987e05387e6fa61
ter <a26214311@gmail.com>
2019-11-28 10:04:13 +0800

    f

--- ai/coin/market.js
+++ ai/coin/market.js
@@ -108,7 +108,7 @@ function getCoinMarket(callback,withproxy, isInterface = false){
     options.agent=agent;
   }
   //options.agent=agent;
-  var req = https.request(options, function(res) {
+  var req = http.request(options, function(res) {
     res.setEncoding('utf8');
     var code = res.statusCode;
     if(code==200){
