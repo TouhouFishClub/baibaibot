b05c0cce10542d0d42100132f369a66e0d1eacb0
ter <a26214311@gmail.com>
2019-12-25 17:47:15 +0800

    f

--- ai/ff14/attr.js
+++ ai/ff14/attr.js
@@ -3,15 +3,20 @@ function calAttr(content,qq,callback){
     var left = content.substring(2).trim();
     var num = parseInt(left);
     var rate = Math.floor((num-380)/3300*55*10)/10;
-    var next = Math.ceil((rate+0.1)*100/100/55*3300+380)
-    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n下一个阈值:"+next);
+    var next = Math.ceil((rate+0.1)*100/100/55*3300+380);
+    var nextrate = rate+0.1;
+    var each = Math.round(((nextrate*1.25+(100-nextrate))-(rate*1.25+(100-rate)))/(rate*1.25+(100-rate))/(next-left)*1000000)/100;
+    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n下一个阈值:"+next+"\n每点收益:"+each);
   }else if(content.startsWith("暴击")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
     var rate = Math.floor(((num-380)/3300*20+5)*10)/10;
     var cx = Math.floor((num-380)/3300*200 +1400)/10;
     var next = Math.ceil((rate-4.9)/20*3300+380);
-    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next);
+    var nextrate = rate+0.1;
+    var nextcx = Math.floor((next-380)/3300*200 +1400)/10;
+    var each = Math.round(((nextrate*nextcx/100+(100-nextrate))-(rate*cx/100+(100-rate)))/(rate*cx/100+(100-rate))/(next-left)*1000000)/100;
+    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next+"\n每点收益:"+each);
   }else if(content.startsWith("速度")||content.startsWith("技速")||content.startsWith("咏速")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
@@ -25,7 +30,9 @@ function calAttr(content,qq,callback){
     var num = parseInt(left);
     var rate = Math.floor(130*(num - 340)/ 3300 + 1000)/10;
     var next = Math.ceil((rate-99.9)*10*3300/130+340);
-    callback('信念收益:'+rate+"%"+"\n下一个阈值:"+next);
+    var nextrate = Math.floor(130*(next - 340)/ 3300 + 1000)/10;
+    var each = Math.round((nextrate-rate)/rate*1000000)/1000
+    callback('信念收益:'+rate+"%"+"\n下一个阈值:"+next+"\n每点收益:"+each);
   }else{
 
   }
