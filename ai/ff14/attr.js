7e6d62f303e47d9f550aed88e9c6d21de0e38ce1
ter <a26214311@gmail.com>
2019-12-25 17:13:38 +0800

    f

--- ai/ff14/attr.js
+++ ai/ff14/attr.js
@@ -12,6 +12,14 @@ function calAttr(content,qq,callback){
     var cx = Math.floor((num-380)/3300*200 +1400)/10;
     var next = Math.ceil((rate-4.9)/20*3300+380);
     callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next);
+  }else if(content.startsWith("速度")||content.startsWith("技速")||content.startsWith("咏速")){
+    var left = content.substring(2).trim();
+    var num = parseInt(left);
+    var rate = Math.floor(130*(num - 380)/ 3300 + 1000);
+    var cx = Math.floor(( 2000 - rate ) * 2500 / 10000)/100;
+    var nextspeed = 2000-(cx-0.01)*100*10000/2500
+    var next = Math.ceil((nextspeed-1000)*3300/130+380)
+    callback('速度:'+rate+""+"\n2.5sGCD:"+cx+"\n下一个阈值:"+next);
   }else{
 
   }
