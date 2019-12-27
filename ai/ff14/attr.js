4282f0a51a9391dd13ed80e651b607ed6c192cdb
ter <a26214311@gmail.com>
2019-12-27 11:54:02 +0800

    f

--- ai/ff14/attr.js
+++ ai/ff14/attr.js
@@ -6,9 +6,11 @@ function calAttr(content,qq,callback){
     var next = Math.ceil((rate+0.1)*100/100/55*3300+380);
     var lastnum = Math.ceil((rate)*100/100/55*3300+380);
     var nextrate = rate+0.1;
-    var each = Math.round(((nextrate*1.25+(100-nextrate))-(rate*1.25+(100-rate)))/(rate*1.25+(100-rate))/(next-lastnum)*10000000)/1000;
+    var nowwin = Math.round((rate*1.25+(100-rate))*100000)/100000;
+    var nextwin = Math.round((nextrate*1.25+(100-nextrate))*100000)/100000;;
+    var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((rate*1.25+(100-rate))*10000/lastnum)/1000;
-    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n下一个阈值:"+next+"\n直击每点收益:"+add+"\n直击每点增益:"+each);
+    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n下一个阈值:"+next+"\n直击收益:"+nowwin+"%\n直击每点收益:"+add+"\n直击每点增益:"+each);
   }else if(content.startsWith("暴击")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
@@ -18,9 +20,11 @@ function calAttr(content,qq,callback){
     var lastnum = Math.ceil((rate-5)/20*3300+380);
     var nextrate = rate+0.1;
     var nextcx = Math.floor((next-380)/3300*200 +1400)/10;
-    var each = Math.round(((nextrate*nextcx/100+(100-nextrate))-(rate*cx/100+(100-rate)))/(rate*cx/100+(100-rate))/(next-lastnum)*10000000)/1000;
+    var nowwin = Math.round((rate*cx/100+(100-rate))*100000)/100000;
+    var nextwin = Math.round((nextrate*nextcx/100+(100-nextrate))*100000)/100000;;
+    var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((rate*cx/100+(100-rate))*10000/lastnum)/1000;
-    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next+"\n暴击每点收益:"+add+"\n暴击每点增益:"+each);
+    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next+"\n暴击收益:"+nowwin+"%\n暴击每点收益:"+add+"\n暴击每点增益:"+each);
   }else if(content.startsWith("速度")||content.startsWith("技速")||content.startsWith("咏速")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
@@ -36,9 +40,11 @@ function calAttr(content,qq,callback){
     var next = Math.ceil((rate-99.9)*10*3300/130+340);
     var lastnum = Math.ceil((rate-100)*10*3300/130+340);
     var nextrate = Math.floor(130*(next - 340)/ 3300 + 1000)/10;
-    var each = Math.round((nextrate-rate)/rate/(next-lastnum)*10000000)/1000
+    var nowwin = rate;
+    var nextwin = nextrate;
+    var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round(rate*10000/lastnum)/1000
-    callback('信念收益:'+rate+"%"+"\n下一个阈值:"+next+"\n信念每点收益:"+add+"\n信念每点增益:"+each);
+    callback('信念收益:'+rate+"%"+"\n下一个阈值:"+next+"\n信念收益:"+nowwin+"%\n信念每点收益:"+add+"\n信念每点增益:"+each);
   }else{
 
   }
