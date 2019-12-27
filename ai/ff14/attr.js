972df4beb897d23715b69fb50b4a78575b38e97d
ter <a26214311@gmail.com>
2019-12-27 15:35:18 +0800

    f

--- ai/ff14/attr.js
+++ ai/ff14/attr.js
@@ -10,7 +10,7 @@ function calAttr(content,qq,callback){
     var nextwin = Math.round((nextrate*1.25+(100-nextrate))*100000)/100000;;
     var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((nowwin-100)*10000/(lastnum-380))/100;
-    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n下一个阈值:"+next+"\n直击收益:"+nowwin+"%\n直击每点收益:"+add+"\n直击每点增益:"+each);
+    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n上一个阈值:"+lastnum+"下一个阈值:"+next+"\n直击收益:"+nowwin+"%\n直击每点收益:"+add+"\n直击每点增益:"+each);
   }else if(content.startsWith("暴击")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
@@ -24,7 +24,7 @@ function calAttr(content,qq,callback){
     var nextwin = Math.round((nextrate*nextcx/100+(100-nextrate))*100000)/100000;;
     var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((nowwin-100)*10000/(lastnum-380))/100;
-    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n下一个阈值:"+next+"\n暴击收益:"+nowwin+"%\n暴击每点收益:"+add+"\n暴击每点增益:"+each);
+    callback('暴击率:'+rate+"%"+"\n暴击伤害:"+cx+"%\n上一个阈值:"+lastnum+"\n下一个阈值:"+next+"\n暴击收益:"+nowwin+"%\n暴击每点收益:"+add+"\n暴击每点增益:"+each);
   }else if(content.startsWith("速度")||content.startsWith("技速")||content.startsWith("咏速")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
@@ -44,7 +44,7 @@ function calAttr(content,qq,callback){
     var nextwin = nextrate;
     var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((nowwin-100)*10000/(lastnum-380))/100;
-    callback('信念收益:'+rate+"%"+"\n下一个阈值:"+next+"\n信念收益:"+nowwin+"%\n信念每点收益:"+add+"\n信念每点增益:"+each);
+    callback('信念收益:'+rate+"%"+"\n上一个阈值:"+lastnum+"\n下一个阈值:"+next+"\n信念收益:"+nowwin+"%\n信念每点收益:"+add+"\n信念每点增益:"+each);
   }else{
 
   }
