297a7cf2da0f1f5ba74c7183e9aad7f55637eb68
ter <a26214311@gmail.com>
2019-12-27 15:36:30 +0800

    f

--- ai/ff14/attr.js
+++ ai/ff14/attr.js
@@ -10,7 +10,7 @@ function calAttr(content,qq,callback){
     var nextwin = Math.round((nextrate*1.25+(100-nextrate))*100000)/100000;;
     var each = Math.round((nextwin-nowwin)/nowwin/(next-lastnum)*10000000)/1000;
     var add = Math.round((nowwin-100)*10000/(lastnum-380))/100;
-    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n上一个阈值:"+lastnum+"下一个阈值:"+next+"\n直击收益:"+nowwin+"%\n直击每点收益:"+add+"\n直击每点增益:"+each);
+    callback('直击率:'+rate+"%"+"\n直击伤害:125%\n上一个阈值:"+lastnum+"\n下一个阈值:"+next+"\n直击收益:"+nowwin+"%\n直击每点收益:"+add+"\n直击每点增益:"+each);
   }else if(content.startsWith("暴击")){
     var left = content.substring(2).trim();
     var num = parseInt(left);
