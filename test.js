21a5dbe566f5b6864ac75fece816c5ec4da81344
时空.blue <a26214311@gmail.com>
2020-01-16 17:27:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-const {  handleGun,  addplayer,  init,  generateImage} = require('./ai/games/survival/main');
-var callback = function(r){console.log(r)}
-handleGun("俄罗斯轮盘","qq1","nam1","g1",callback)
-addplayer("qq1","nam1","g1",callback);
-addplayer("qq2","nam2","g1",callback);
-addplayer("qq3","nam3","g1",callback);
-addplayer("qq4","nam4","g1",callback);
-addplayer("qq5","nam5","g1",callback);
-addplayer("qq6","nam6","g1",callback);
-init(callback);
+
