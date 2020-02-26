a0a0cb0ec5f136dfeb266f199bc92cbb0810cf49
flandre <flantan233@gmail.com>
2020-02-26 15:09:19 +0800

    add calendar

--- ai/arknights/arkNightsCalendar.js
+++ ai/arknights/arkNightsCalendar.js
@@ -1,3 +1,186 @@
-module.exports = function(callback) {
-  
+const fs = require('fs-extra')
+const path = require('path-extra')
+
+const calendar = callback => {
+  const define = fs.readJsonSync(path.join(__dirname, 'calendarDefine.json'))
+  const today = new Date()
+  const iday = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate()
+  renderImage(
+    pickTodaysLuck(iday, define),
+    getDirectionString(define, iday),
+    getDrinkString(define),
+    getStarString(iday),
+    callback
+  )
+}
+
+const random = (dayseed, indexseed) => {
+  var n = dayseed % 11117;
+  for (var i = 0; i < 100 + indexseed; i++) {
+    n = n * n;
+    n = n % 11117; // 11117 是个质数
+  }
+  return n;
+}
+
+const renderImage = (pick, direct, drink, star, callback) => {
+
+
+}
+
+function star(num) {
+  var result = [];
+  var i = 0;
+  while (i < num) {
+    result.push('★');
+    i++;
+  }
+  while (i < 5) {
+    result.push('☆');
+    i++;
+  }
+  return result.join('');
+}
+
+// 生成今日运势
+function pickTodaysLuck(iday, define) {
+  let _activities = filter(define.activities);
+
+  let numGood = random(iday, 98) % 3 + 2;
+  let numBad = random(iday, 87) % 3 + 2;
+  let eventArr = pickRandomActivity(_activities, numGood + numBad, iday, define);
+
+  let special = pickSpecials(iday, define);
+
+  let goodList = special.good;
+  let badList = special.bad;
+
+  for (let i = 0; i < numGood; i++) {
+    goodList.push(eventArr[i]);
+  }
+
+  for (let i = 0; i < numBad; i++) {
+    badList.push(eventArr[numGood + i]);
+  }
+
+  return {good: goodList.slice(0, 4), bad: badList.slice(0, 4)};
+}
+
+// 去掉一些不合今日的事件
+function filter(activities) {
+  let result = [];
+
+  // 周末的话，只留下 weekend = true 的事件
+  if (isWeekend(new Date())) {
+
+    for (let i = 0; i < activities.length; i++) {
+      if (activities[i].weekend) {
+        result.push(activities[i]);
+      }
+    }
+
+    return result;
+  }
+
+  return activities;
+}
+
+function isWeekend(today) {
+  return today.getDay() == 0 || today.getDay() == 6;
+}
+
+// 添加预定义事件
+function pickSpecials(iday, define) {
+
+  let goodList = [];
+  let badList = [];
+
+  for (let i = 0; i < define.specials.length; i++) {
+    let special = define.specials[i];
+
+    if (iday == special.date) {
+      if (special.type == 'good') {
+        goodList.push({
+          name: special.name,
+          good: special.description
+        });
+      } else {
+        badList.push({
+          name: special.name,
+          bad: special.description
+        });
+      }
+    }
+  }
+
+  return {good: goodList, bad: badList};
+}
+
+// 从 activities 中随机挑选 size 个
+function pickRandomActivity(activities, size, iday, define) {
+  let picked_events = pickRandom(activities, size, iday);
+
+  for (let i = 0; i < picked_events.length; i++) {
+    picked_events[i] = parse(picked_events[i], define, iday);
+  }
+
+  return picked_events;
+}
+
+// 从数组中随机挑选 size 个
+function pickRandom(array, size, iday) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i]);
+  }
+
+  for (let j = 0; j < array.length - size; j++) {
+    let index = random(iday, j) % result.length;
+    result.splice(index, 1);
+  }
+
+  return result;
+}
+
+// 解析占位符并替换成随机内容
+function parse(event, define, iday) {
+  let result = {
+    name: event.name,
+    good: event.good,
+    bad: event.bad
+  }; // clone
+
+  if (result.name.indexOf('%v') != -1) {
+    result.name = result.name.replace('%v', define.varNames[random(iday, 12) % define.varNames.length]);
+  }
+
+  if (result.name.indexOf('%t') != -1) {
+    result.name = result.name.replace('%t', define.tools[random(iday, 11) % define.tools.length]);
+  }
+
+  if (result.name.indexOf('%l') != -1) {
+    result.name = result.name.replace('%l', (random(iday, 12) % 247 + 30).toString());
+  }
+
+  return result;
+}
+
+// 获得座位朝向
+function getDirectionString(define, iday) {
+  return define.directions[random(iday, 2) % define.directions.length];
+}
+
+// 获得今日宜饮
+function getDrinkString(define) {
+  return pickRandom(define.drinks, 2).join('，');
+}
+
+// 获得女神亲近指数
+function getStarString(iday) {
+  return star(random(iday, 6) % 5 + 1);
+}
+
+module.exports = {
+  calendar
 }
