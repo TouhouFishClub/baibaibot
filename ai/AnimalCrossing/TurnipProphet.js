ca5458976d266939e0c25b4c61d84e8bbebd47cc
flandre <flantan233@gmail.com>
2020-04-21 11:59:14 +0800

    update

--- ai/AnimalCrossing/TurnipProphet.js
+++ ai/AnimalCrossing/TurnipProphet.js
@@ -2,7 +2,7 @@ const { analyze_possibilities } = require('./core/predictions')
 const render = require('./renderImage')
 const { getUserDTCInfo } = require('./priceRecord')
 
-const testJson = { "_id" : 357474405, "gid" : [ 205700800, 221698514, 102135023 ], "d" : { "18369" : { "p0" : 121, "p1" : 109 }, "18370" : { "p0" : 71, "p1" : 0 }, "18371" : { "p0" : 97, "p1" : 0 }, "18372" : { "p0" : 54, "p1" : 50 }, "18373" : { "p0" : 44, "p1" : 0 } }, "tz" : 16 }
+// const testJson = { "_id" : 357474405, "gid" : [ 205700800, 221698514, 102135023 ], "d" : { "18369" : { "p0" : 121, "p1" : 109 }, "18370" : { "p0" : 71, "p1" : 0 }, "18371" : { "p0" : 97, "p1" : 0 }, "18372" : { "p0" : 54, "p1" : 50 }, "18373" : { "p0" : 44, "p1" : 0 } }, "tz" : 16 }
 
 const help = callback => {
   callback('这是帮助')
@@ -12,25 +12,8 @@ const formatSaveData = (data, type, callback) => {
   let d = data.d
 
   var tz = 16;
-  var n = content.indexOf("utc");
-  if(data) {
-    if (n >= 0) {
-      tz = 2 * parseInt(content.substring(n + 3));
-      content = content.substring(0, n);
-      if (tz > 24 || tz < -24) {
-        tz = 16;
-      }
-    } else {
-      tz = data.tz;
-    }
-  }else {
-    if (n >= 0) {
-      tz = 2 * parseInt(content.substring(n + 3));
-      content = content.substring(0, n);
-      if (tz > 24 || tz < -24) {
-        tz = 16;
-      }
-    }
+  if(typeof data.tz == 'number') {
+    tz = data.tz
   }
 
   var now = new Date();
@@ -40,25 +23,45 @@ const formatSaveData = (data, type, callback) => {
   var pd = (day+4)%7;
 
 
-  let ret = ''
+  let outArr = []
   for(var i=pd;i>=0;i--){
-    var thenprice = d[day-i]?(d[day-i].p0+"-"+d[day-i].p1):"0-0";
-    ret = ret + "周"+(pd-i)+":"+thenprice+"\n";
+    if(i == pd) {
+      if(d[day - i]){
+        outArr.push(d[day - i].p0 || NaN)
+        outArr.push(d[day - i].p0 || NaN)
+      } else {
+        outArr.push(NaN)
+        outArr.push(NaN)
+      }
+    } else {
+      if(d[day - i]){
+        outArr.push(d[day - i].p0 || NaN)
+        outArr.push(d[day - i].p1 || NaN)
+      } else {
+        outArr.push(NaN)
+        outArr.push(NaN)
+      }
+    }
   }
-
+  let calc = analyze_possibilities(outArr, false, -1)
+  render(calc, callback)
 }
 
 const findSaveData = (qq, type, callback) => {
   getUserDTCInfo(qq, data => {
     if(data && data.d) {
-
+      formatSaveData(data, type, callback)
+    } else {
+      help()
     }
   })
 }
 
 module.exports = function(content, qq, group, type, callback) {
   if(content.trim() == ''){
-    help()
+    findSaveData(qq, type, callback)
+    // formatSaveData(testJson, type, callback)
+    // help(callback)
     return
   }
   let format = content.replace(/ +/g, ' ').trim().split(' ')
