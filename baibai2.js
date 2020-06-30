1d4a3d405777f0720bb56713b0380a1aa8edfa5f
flandre <flantan233@gmail.com>
2020-06-30 14:21:12 +0800

    test

--- baibai2.js
+++ baibai2.js
@@ -573,10 +573,10 @@ function handleMsg_D0(msgObj,botqq){
 
 
 function handleMsg_D(msgObj,botqq) {
-  console.log('\n======================')
-  console.log(msgObj)
-  console.log(botqq)
-  console.log('======================\n')
+  // console.log('\n======================')
+  // console.log(msgObj)
+  // console.log(botqq)
+  // console.log('======================\n')
   var type = msgObj.message_type;
   var groupid = msgObj.group_id;
   var content = msgObj.message;
