2dce6248df89b5c3aa51835c628e36d3bc1c1bfe
flandre <flantan233@gmail.com>
2020-08-17 17:10:52 +0800

    fix bug

--- baibai2.js
+++ baibai2.js
@@ -901,12 +901,15 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
     chp(callback)
   }
 
-  if(con.substring(0, 2) == 'mc') {
-    morse(con.substring(2).trim(), true, callback)
+
+  if(con.substring(0, 3) == 'mct') {
+    morse(con.substring(3).trim(), false, callback)
+    return
   }
 
-  if(con.substring(0, 2) == 'mt') {
-    morse(con.substring(2).trim(), false, callback)
+  if(con.substring(0, 2) == 'mc') {
+    morse(con.substring(2).trim(), true, callback)
+    return
   }
 
   let fie4 = con.substring(0, 4)
