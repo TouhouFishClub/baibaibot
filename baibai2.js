007efe1d45d60b87f94d8f8a10df96bc76f3d012
ter <a26214311@gmail.com>
2020-08-21 17:20:39 +0800

    f

--- baibai2.js
+++ baibai2.js
@@ -1048,7 +1048,7 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
   }
 
 
-  if(port!=23334){
+  if(port!=22222){
     if(rcontent.startsWith("炼铜1")){
       copperReply(rcontent,groupid,from,callback);
       return;
