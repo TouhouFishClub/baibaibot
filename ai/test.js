bdc496cf1560426359bf44734d14917c33ffdf11
flandre <flantan233@gmail.com>
2020-08-31 15:17:59 +0800

    update

--- ai/test.js
+++ ai/test.js
@@ -1,5 +1,5 @@
 // const akc = require('./CoV2020')
-// const {cov} = require('./CoV2019ByTianApi')
+const {cov} = require('./CoV2019ByTianApi')
 const mc = require('./MorseCode')
 const c = m => {
   console.log('===== output =====')
@@ -7,6 +7,24 @@ const c = m => {
 }
 
 // chp(c)
-setTimeout(() => {
-  mc('adfadfasd/.-./.-./-.-/.-/.../--./.-/--./.-', false, c)
-}, 1)
+cov(
+    '硝局的花园',
+    c,
+    true,
+    ['现有女友', '今日新增女友', '确诊女友', '疑似女友', '境外女友'],
+    {
+      name: '硝局的花园',
+      type: 'other',
+    },
+    {
+      confirmedCount: [0, 0],
+      curedCount: [114514, 0],
+      currentConfirmedCount: [0, 0],
+      deadCount: [0, 0],
+      suspectedCount: [0, 0]
+    },
+    '硝局的花园',
+  )
+// setTimeout(() => {
+//   mc('adfadfasd/.-./.-./-.-/.-/.../--./.-/--./.-', false, c)
+// }, 1)
--- baibai2.js
+++ baibai2.js
@@ -1023,6 +1023,28 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
   let co = content.indexOf('疫情')
   if(co >= 0 && co <= 6 && content.length - co == 2) {
     cov(content.substring(0, co), callback)
+    return
+  }
+  if(content == '硝局的花园') {
+    cov(
+      '硝局的花园',
+      c,
+      true,
+      ['现有女友', '今日新增女友', '确诊女友', '疑似女友', '境外女友'],
+      {
+        name: '硝局的花园',
+        type: 'other',
+      },
+      {
+        confirmedCount: [0, 0],
+        curedCount: [114514, 0],
+        currentConfirmedCount: [0, 0],
+        deadCount: [0, 0],
+        suspectedCount: [0, 0]
+      },
+      '硝局的花园',
+    )
+    return
   }
 
   if(n>1&&n<10&&rcontent.length==n+2){
