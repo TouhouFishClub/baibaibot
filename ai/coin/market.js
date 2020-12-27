4f5c892934cff4d1bced59205df43c5a4ecd6849
时空.blue <a26214311@gmail.com>
2020-12-27 20:02:53 +0800

    Update market.js

--- ai/coin/market.js
+++ ai/coin/market.js
@@ -168,7 +168,7 @@ function getCoinMarket(callback,withproxy, isInterface = false){
     console.log('req err:');
     console.log(err);
     failed = failed + 1;
-    if(failed>2){
+    if(failed>3){
       if(isInterface){
         callback([])
       }else{
@@ -178,7 +178,7 @@ function getCoinMarket(callback,withproxy, isInterface = false){
       getCoinMarket(callback,false,isInterface);
     }
   });
-  req.setTimeout(5000,function(){
+  req.setTimeout(8000,function(){
     failed = failed + 1;
     if(failed>2){
       callback('CoinMarket BOOM!');
