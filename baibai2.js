de0bc6be1007d712aed0cca1ed75d41cb6cb9ee9
flandre <flantan233@gmail.com>
2021-04-22 11:20:00 +0800

    test

--- baibai2.js
+++ baibai2.js
@@ -643,6 +643,7 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
   }
 
   let FLASH_RESEND = false
+	console.log(from)
 
   if(content.startsWith == '/' && from == 799018865) {
   	console.log('=============================')
