7c05085c358975066487fd288a2f1019f5b2f840
flandre <flantan233@gmail.com>
2021-04-22 14:37:16 +0800

    fix bug

--- MsgHandler/flash.js
+++ MsgHandler/flash.js
@@ -4,40 +4,44 @@ const path = require('path-extra')
 const basicPath = '/home/hduser/upload/tk/mb'
 
 const flashHandler = (qq, groupid, cacheFile, port, isResend, callback) => {
-	var options = {
-		host: '192.168.17.52',
-		port: port,
-		path: `/get_image?file=${cacheFile}`,
-		method: 'GET',
-		headers: {}
-	};
-	var req = http.request(options, res => {
-		let resdata = ''
-		res.on('data', chunk => {
-			resdata = resdata + chunk
-		})
-		res.on('end', () => {
-			if(resdata.length > 0){
-				try {
-					let d = JSON.parse(resdata)
-					let filePath = d.data.file
-					if(isResend) {
-						callback(`检测到闪照：[CQ:image,file=http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 10000000000)}-${cacheFile}/0?term=3`)
-					} else {
-						// callback(`检测到闪照，我自己偷偷存下了`)
-					}
-				} catch {
-					console.log('ERROR: 【闪照】转换JSON错误')
-				}
-				// callback(`port: ${port}\ndata: ${resdata}`);
-			}
-		})
-	})
-	req.on('error', err => {
-		console.log('req err:')
-		console.log(err)
-	})
-	req.end()
+
+
+	if(isResend) {
+		callback(`检测到闪照：[CQ:image,file=http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 10000000000)}-${cacheFile}/0?term=3`)
+	} else {
+		// callback(`检测到闪照，我自己偷偷存下了`)
+	}
+
+
+	// var options = {
+	// 	host: '192.168.17.52',
+	// 	port: port,
+	// 	path: `/get_image?file=${cacheFile}`,
+	// 	method: 'GET',
+	// 	headers: {}
+	// };
+	// var req = http.request(options, res => {
+	// 	let resdata = ''
+	// 	res.on('data', chunk => {
+	// 		resdata = resdata + chunk
+	// 	})
+	// 	res.on('end', () => {
+	// 		if(resdata.length > 0){
+	// 			try {
+	// 				let d = JSON.parse(resdata)
+	// 				let filePath = d.data.file
+	// 			} catch {
+	// 				console.log('ERROR: 【闪照】转换JSON错误')
+	// 			}
+	// 			// callback(`port: ${port}\ndata: ${resdata}`);
+	// 		}
+	// 	})
+	// })
+	// req.on('error', err => {
+	// 	console.log('req err:')
+	// 	console.log(err)
+	// })
+	// req.end()
 }
 module.exports = {
 	flashHandler
