b652f9dd96faa71e07eeb2cb503e3bb271924e6a
Flandre <flantan233@gmail.com>
2021-05-01 16:55:45 +0800

    update

---
+++ ai/chat/5000choyen.js
@@ -0,0 +1,12 @@
+// source https://github.com/yurafuca/5000choyen
+const { sendImageMsgBuffer } = require('../../cq/sendImage')
+const {createCanvas, loadImage} = require('canvas')
+
+const fiveThousandTrillionYen = (content, callback) => {
+
+
+}
+
+module.exports = {
+  fiveThousandTrillionYen
+}
