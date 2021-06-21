c6f82273f2510005ac4c46b5554a05bc2581f411
flandre <flantan233@gmail.com>
2021-06-21 09:53:04 +0800

    reset theme

--- ai/mabinogi/tools/optionsetImage.js
+++ ai/mabinogi/tools/optionsetImage.js
@@ -4,18 +4,8 @@ const fs = require('fs'),
   { sendImageMsgBuffer } = require('../../../cq/sendImage.js')
 // const gm = require('gm')
 // let imageMagick = gm.subClass({ imageMagick : true });
-// const theme = {
-// 	bg: 'rgba(0,0,20,0.9)',
-// 	buff: 'rgba(16,131,255,1)',
-// 	debuff: 'rgba(251,0,7,1)',
-// 	border: 'rgba(204,204,204,1)',
-// 	tagbg: 'rgba(0,0,0,1)',
-// 	tag: 'rgba(238,78,7,1)',
-// 	text: 'rgba(255,255,255,1)',
-// }
-
 const theme = {
-	bg: '#ff96ae',
+	bg: 'rgba(0,0,20,0.9)',
 	buff: 'rgba(16,131,255,1)',
 	debuff: 'rgba(251,0,7,1)',
 	border: 'rgba(204,204,204,1)',
@@ -23,6 +13,16 @@ const theme = {
 	tag: 'rgba(238,78,7,1)',
 	text: 'rgba(255,255,255,1)',
 }
+
+// const theme = {
+// 	bg: '#ff96ae',
+// 	buff: 'rgb(16,131,255)',
+// 	debuff: 'rgba(251,0,7,1)',
+// 	border: 'rgba(204,204,204,1)',
+// 	tagbg: 'rgba(0,0,0,1)',
+// 	tag: 'rgba(238,78,7,1)',
+// 	text: 'rgba(255,255,255,1)',
+// }
 
 const checkMaxWidth = (ctx, str, maxWidth) => {
   let start = 0, splitArr = []
