60ea4c935c7c71d82e2d1bb93118cc01c6508fc5
Flandre <flantan233@gmail.com>
2021-10-19 17:13:17 +0800

    ignore mc module

--- baibai2.js
+++ baibai2.js
@@ -1063,15 +1063,15 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
   }
 
 
-  if(con.substring(0, 3) == 'mct') {
-    morse(con.substring(3).trim(), false, callback)
-    return
-  }
-
-  if(con.substring(0, 2) == 'mc') {
-    morse(con.substring(2).trim(), true, callback)
-    return
-  }
+  // if(con.substring(0, 3) == 'mct') {
+  //   morse(con.substring(3).trim(), false, callback)
+  //   return
+  // }
+  //
+  // if(con.substring(0, 2) == 'mc') {
+  //   morse(con.substring(2).trim(), true, callback)
+  //   return
+  // }
 
   let fie4 = con.substring(0, 4)
   if(fie4 == '切噜～♪') {
