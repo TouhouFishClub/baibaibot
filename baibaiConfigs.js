6adb60ee509338d9497db65564d0c10167439e84
ter <a26214311@gmail.com>
2022-02-28 17:23:10 +0800

    w

--- baibaiConfigs.js
+++ baibaiConfigs.js
@@ -4,11 +4,13 @@ let RECORD_DATA = path.join(__dirname, '../coolq-data/cq/data/record/send')
 let TEST = 'baibai'
 var mongourl = 'mongodb://192.168.17.52:27050/db_bot';
 var mongoff14url = 'mongodb://192.168.17.52:27050/db_ff14';
+var myip = '192.168.17.236';
 
 module.exports = {
   IMAGE_DATA,
   RECORD_DATA,
   TEST,
   mongourl,
-  mongoff14url
+  mongoff14url,
+  myip
 }
