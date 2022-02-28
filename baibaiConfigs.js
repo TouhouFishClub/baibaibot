016e208efa1d437cd8df15318995970e65df59ec
ter <a26214311@gmail.com>
2022-02-28 16:30:55 +0800

    w

--- baibaiConfigs.js
+++ baibaiConfigs.js
@@ -2,9 +2,13 @@ const path = require('path')
 let IMAGE_DATA = path.join(__dirname, '../coolq-data/cq/data/image/send')
 let RECORD_DATA = path.join(__dirname, '../coolq-data/cq/data/record/send')
 let TEST = 'baibai'
+var mongourl = 'mongodb://192.168.17.52:27050/db_bot';
+var mongoff14url = 'mongodb://192.168.17.52:27050/db_ff14';
 
 module.exports = {
   IMAGE_DATA,
   RECORD_DATA,
-  TEST
+  TEST,
+  mongourl,
+  mongoff14url
 }
