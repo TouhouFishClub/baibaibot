1deb724a44519f7171a44cacb362c6dc4f5f22fc
ter <a26214311@gmail.com>
2022-03-01 10:08:40 +0800

    w

--- baibaiConfigs.js
+++ baibaiConfigs.js
@@ -2,8 +2,8 @@ const path = require('path')
 let IMAGE_DATA = path.join(__dirname, '../coolq-data/cq/data/image/send')
 let RECORD_DATA = path.join(__dirname, '../coolq-data/cq/data/record/send')
 let TEST = 'baibai'
-var mongourl = 'mongodb://192.168.17.52:27050/db_bot';
-var mongoff14url = 'mongodb://192.168.17.52:27050/db_ff14';
+var mongourl = 'mongodb://192.168.17.236:27050/db_bot';
+var mongoff14url = 'mongodb://192.168.17.236:27050/db_ff14';
 var myip = '192.168.17.236';
 
 module.exports = {
