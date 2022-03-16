817cb09c81d1fdcd857cc2e1b9db92604884a8a5
Flandre <flantan233@gmail.com>
2022-03-16 14:34:36 +0800

    update render color

--- ai/mabinogi/renderColor.js
+++ ai/mabinogi/renderColor.js
@@ -2,16 +2,24 @@ const { sendImageMsgBuffer } = require('../../cq/sendImage')
 const {createCanvas, registerFont} = require('canvas')
 
 const renderColorBoard = (context, callback) => {
+  let c = ''
+  if(context.match(/\d{1,3}[,， ]\d{1,3}[,， ]\d{1,3}/)) {
+    let cp = context.replace(/[， ]/g, ',').split(',')
+    if(cp.filter(x => parseInt(x) < 256).length == 3){
+      c = `rgb(${cp.join(',')})`
+    }
+  }
   if((context.length == 6 || context.length == 3) && context.match(/^[0-9ABCDEFabcdef]*$/)) {
-    let c = context.toUpperCase()
+    c = context.toUpperCase()
     if(c.length == 3) {
       c = c.split('').map(x => `${x}${x}`).join('')
     }
-
-
+    c = `#${c}`
+  }
+  if(c) {
     let canvas = createCanvas(100, 20)
       , ctx = canvas.getContext('2d')
-    ctx.fillStyle = `#${c}`
+    ctx.fillStyle = `${c}`
     ctx.fillRect(0, 0, 100, 20)
 
     let imgData = canvas.toDataURL()
@@ -21,7 +29,6 @@ const renderColorBoard = (context, callback) => {
     sendImageMsgBuffer(dataBuffer, 'output', 'other', msg => {
       callback(msg)
     }, `${context}是这个颜色:`, 'MF')
-
   } else {
     callback(`${context} 输入错误`)
   }
