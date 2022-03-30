2c2be5217bd7703a7a55f6ad8f55fe3a209d6265
Flandre <flantan233@gmail.com>
2022-03-30 21:50:26 +0800

    update

--- ai/mabinogi/gacha/statistics.js
+++ ai/mabinogi/gacha/statistics.js
@@ -1,12 +1,24 @@
 const http = require('http')
 const qs = require('qs')
 
+const addZero = num => num < 10 ? ('0' + num) : num
+
+const DateToStr = date => `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())} ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`
+
+const fixStrLength = (targetLength, str) => {
+  let sl =  str.replace(/[^\u0000-\u00ff]/g, "aa").length
+  if (sl < targetLength) {
+    return `${str}${new Array(targetLength - sl).fill(' ').join('')}`
+  }
+  return str
+}
+
 const fetchData = ( page ) =>
   new Promise((resolve, reject) => {
     const options = {
       host: 'mabinogimirai.natapp1.cc',
       port: 80,
-      path: '/News/eggData',
+      path: '/News/baiEggNewsData',
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -14,10 +26,12 @@ const fetchData = ( page ) =>
     }
     const obj = {
       player: '',
-      token: '', //Mirai's 全数据
+      // token: '', //Mirai's 全数据
       content: '',
       page,
-      // server: ''
+      server: '伊文',
+      start: '2022-03-30 11:30:00',
+      end: DateToStr(new Date())
     }
     const req = http.request(options, res => {
       res.setEncoding('utf8')
@@ -26,6 +40,7 @@ const fetchData = ( page ) =>
         resData += chunk
       });
       res.on('end', () => {
+        // console.log(resData)
         resolve(JSON.parse(resData))
       })
     })
@@ -44,6 +59,7 @@ const fetchAllData = async () => {
     if(data && data.length) {
       res = res.concat(data)
       page ++
+      flag = false
     } else {
       flag = false
     }
@@ -57,17 +73,21 @@ const statistics = async () => {
   let timeLeftTs = new Date('2022-3-30 11:30:00').getTime()
   let data = await fetchAllData()
   let SRankMap = new Map(), count = 0
-  data.filter(x => new Date(x.createTime).getTime() > timeLeftTs).reverse().forEach((list, index) => {
-    let { item } = list
+  data.filter(x => new Date(x.createTime).getTime() > timeLeftTs).forEach((list, index) => {
+    let { item, createTime, player } = list
     if(SRankMap.get(item)) {
       SRankMap.set(item, {
         count : SRankMap.get(item).count + 1,
-        last: index
+        last: index,
+        lastTime: createTime,
+        lastUser: player
       })
     } else {
       SRankMap.set(item, {
         count : 1,
-        last: index
+        last: index,
+        lastTime: createTime,
+        lastUser: player
       })
     }
     count ++
@@ -80,7 +100,9 @@ const statistics = async () => {
       name,
       count: info.count,
       rareStr: (info.count / count * 100).toFixed(2),
-      last: count - info.last
+      last: count - info.last,
+      lastTime: info.lastTime,
+      lastUser: fixStrLength(12, info.lastUser)
     })
     console.log(`概率: ${(info.count / count * 100).toFixed(2)}%\t次数: ${fixNumber(info.count)}\t最后一次上电视: ${fixNumber(count - info.last)}次前\t${name}`)
   })
@@ -88,13 +110,14 @@ const statistics = async () => {
   out.sort((a, b) => b.count - a.count)
   console.log('==== 按出率 ===')
   out.forEach(item => {
-    console.log(`概率: ${item.rareStr}%\t次数: ${fixNumber(item.count)}\t最后一次上电视: ${fixNumber(item.last)}次前\t\t${item.name}`)
+    console.log(`概率: ${item.rareStr}%\t次数: ${fixNumber(item.count)}\t最后电视: ${fixNumber(item.last)}次前(${item.lastTime} | ${item.lastUser})\t${item.name}`)
   })
   out.sort((a, b) => b.last - a.last)
   console.log('==== 按最后上电视 ===')
   out.forEach(item => {
-    console.log(`概率: ${item.rareStr}%\t次数: ${fixNumber(item.count)}\t最后一次上电视: ${fixNumber(item.last)}次前\t\t${item.name}`)
+    console.log(`概率: ${item.rareStr}%\t次数: ${fixNumber(item.count)}\t最后电视: ${fixNumber(item.last)}次前(${item.lastTime} | ${item.lastUser})\t${item.name}`)
   })
 }
 
+// fetchData(2)
 statistics()
