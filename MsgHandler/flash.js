5ec4ffc1b075b26ae8578b7a181594022d965f94
Flandre <flantan233@gmail.com>
2022-04-27 22:13:50 +0800

    fix flash resend

--- MsgHandler/flash.js
+++ MsgHandler/flash.js
@@ -5,10 +5,10 @@ const basicPath = '/home/hduser/upload/tk/mb'
 
 const flashHandler = (qq, groupid, cacheFile, port, isResend, callback) => {
 
-	let url = `http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 10000000000)}-${cacheFile}/0?term=3`
+	let url = `http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 1000000000)}-${cacheFile}/0??term=255,subType=1`
 
 	if(isResend) {
-		callback(`检测到闪照：[CQ:image,file=${cacheFile}.image,url=http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 10000000000)}-${cacheFile.toUpperCase()}/0?term=3]`)
+		callback(`检测到闪照：[CQ:image,file=${cacheFile}.image,url=http://gchat.qpic.cn/gchatpic_new/${qq}/${groupid}-${~~(Math.random() * 1000000000)}-${cacheFile.toUpperCase()}/0?term=255,subType=1]`)
 	} else {
 		// callback(`检测到闪照，我自己偷偷存下了`)
 	}
