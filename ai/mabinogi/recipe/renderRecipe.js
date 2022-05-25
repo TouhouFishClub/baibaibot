e3c390fecb4be8bb35cc602e153a20ac7b7fe1bd
Flandre <flantan233@gmail.com>
2022-05-25 16:54:59 +0800

    fix style

--- ai/mabinogi/recipe/renderRecipe.js
+++ ai/mabinogi/recipe/renderRecipe.js
@@ -135,6 +135,11 @@ const renderRecipeImage = (html, name, showDesc = false, callback, msg = '', ord
       /*  -webkit-text-shadow: #000 1px 0 0, #000 0 1px 0, #000 -1px 0 0, #000 0 -1px 0;*/
       /*  -moz-text-shadow: #000 1px 0 0, #000 0 1px 0, #000 -1px 0 0, #000 0 -1px 0*/
       /*}*/
+      .custom-td {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
       .chara-info {
         color: gold;
         font-size: 12px;
