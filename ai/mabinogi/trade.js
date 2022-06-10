0cb9356f632d2c58b49a68a6e4cbd9a52e90f2af
Flandre <flantan233@gmail.com>
2022-06-11 04:48:47 +0800

    update

--- ai/mabinogi/trade.js
+++ ai/mabinogi/trade.js
@@ -1,138 +1,91 @@
 
 const AREAS = {
   'DEK': {
-    name: '',
+    name: '迪尔科内尔',
     goods: [
       {
         level: 1,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '儿童药水',
+        blockLimit: 80,
+        weight: 1
       },
       {
         level: 2,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '减肥药水',
+        blockLimit: 100,
+        weight: 1
       },
       {
         level: 3,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '止呼噜药',
+        blockLimit: 100,
+        weight: 2
       },
       {
         level: 4,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '人参药水',
+        blockLimit: 50,
+        weight: 3
       },
       {
         level: 5,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '爱情药水',
+        blockLimit: 100,
+        weight: 3
       }
     ],
     timesQuery: [
-      { id: 'DEK', time: },
-      { id: 'DBL', time: },
-      { id: 'BGE', time: },
-      { id: 'AMM', time: },
-      { id: 'TTI', time: },
-      { id: 'TLA', time: },
-      { id: 'KPU', time: },
-      { id: 'BEF', time: },
-    ]
-  },
-  'DEK': {
-    name: '',
-    goods: [
-      {
-        level: 1,
-        name: '',
-        blockLimit: ,
-        weight:
-      },
-      {
-        level: 2,
-        name: '',
-        blockLimit: ,
-        weight:
-      },
-      {
-        level: 3,
-        name: '',
-        blockLimit: ,
-        weight:
-      },
-      {
-        level: 4,
-        name: '',
-        blockLimit: ,
-        weight:
-      },
-      {
-        level: 5,
-        name: '',
-        blockLimit: ,
-        weight:
-      }
-    ],
-    timesQuery: [
-      { id: 'DEK', time: },
-      { id: 'DBL', time: },
-      { id: 'BGE', time: },
-      { id: 'AMM', time: },
-      { id: 'TTI', time: },
-      { id: 'TLA', time: },
-      { id: 'KPU', time: },
-      { id: 'BEF', time: },
+      { id: 'DBL', time: 180 },
+      { id: 'BGE', time: 370 },
+      { id: 'AMM', time: 300 },
+      { id: 'TTI', time: 213 },
+      { id: 'TLA', time: 461 },
+      { id: 'KPU', time: 200 },
+      { id: 'BEF', time: 535 },
     ]
   },
   'DBL': {
-    name: '',
+    name: '敦巴伦',
     goods: [
       {
         level: 1,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '蜘蛛丝手套',
+        blockLimit: 10,
+        weight: 5
       },
       {
         level: 2,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '羊毛靴',
+        blockLimit: 10,
+        weight: 8
       },
       {
         level: 3,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '食人魔屠夫假面',
+        blockLimit: 50,
+        weight: 4
       },
       {
         level: 4,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '男妖正装',
+        blockLimit: 5,
+        weight: 25
       },
       {
         level: 5,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '女妖泳衣',
+        blockLimit: 10,
+        weight: 6
       }
     ],
     timesQuery: [
-      { id: 'DEK', time: },
-      { id: 'DBL', time: },
-      { id: 'BGE', time: },
-      { id: 'AMM', time: },
-      { id: 'TTI', time: },
-      { id: 'TLA', time: },
-      { id: 'KPU', time: },
-      { id: 'BEF', time: },
+      { id: 'DEK', time: 180 },
+      { id: 'BGE', time: 195 },
+      { id: 'AMM', time: 139 },
+      { id: 'TTI', time: 235 },
+      { id: 'TLA', time: 430 },
+      { id: 'KPU', time: 40 },
+      { id: 'BEF', time: 390 },
     ]
   },
   'BGE': {
@@ -361,53 +314,56 @@ const AREAS = {
     ]
   },
   'BEF': {
-    name: '',
+    name: '贝尔法斯特',
     goods: [
       {
         level: 1,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '铁鞭',
+        blockLimit: 15,
+        weight: 8
       },
       {
         level: 2,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '黑光剑',
+        blockLimit: 10,
+        weight: 12
       },
       {
         level: 3,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '金库',
+        blockLimit: 1,
+        weight: 220
       },
       {
         level: 4,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '骷髅食人魔铠甲',
+        blockLimit: 1,
+        weight: 180
       },
       {
         level: 5,
-        name: '',
-        blockLimit: ,
-        weight:
+        name: '莫蓝特头盔仿品',
+        blockLimit: 10,
+        weight: 40
       }
     ],
     timesQuery: [
-      { id: 'DEK', time: },
-      { id: 'DBL', time: },
-      { id: 'BGE', time: },
-      { id: 'AMM', time: },
-      { id: 'TTI', time: },
-      { id: 'TLA', time: },
-      { id: 'KPU', time: },
-      { id: 'BEF', time: },
+      { id: 'DEK', time: 535 },
+      { id: 'DBL', time: 390 },
+      { id: 'BGE', time: 506 },
+      { id: 'AMM', time: 500 },
+      { id: 'TTI', time: 610 },
+      { id: 'TLA', time: 790 },
+      { id: 'KPU', time: 326 }
     ]
-  },
+  }
 }
 
 
 const trade = (content, qq, groupId, callback) => {
 
+}
+
+module.exports = {
+  trade
 }
