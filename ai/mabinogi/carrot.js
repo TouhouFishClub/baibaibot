7ee9a94d5e7b09e1237e4d7cde0686ee62d8ddeb
Flandre <flantan233@gmail.com>
2022-06-27 14:10:30 +0800

    add mabinogi carrot

---
+++ ai/mabinogi/carrot.js
@@ -0,0 +1,51 @@
+const https = require("https");
+
+const carrot = async callback => {
+	let res = fetchData()
+	console.log('end', splitText(res, 'var end', ';'))
+	console.log('now', splitText(res, 'var now', ';'))
+	res.split('当前胡萝卜售价').slice(1).map(str => {
+		console.log(splitText(str, '<span>', '</span>', true))
+	})
+}
+
+const splitText = (str, start, end = '', ignore = false) => {
+	if(str.length == '') {
+		return str
+	}
+	let si = str.indexOf(start), ei = end.length ? str.indexOf(end) : str.length
+	if(si < 0) {
+		si = 0
+		start = ''
+	}
+	if(ei < 0) {
+		ei = str.length
+		end = ''
+	}
+	return ignore ? str.substring(si + start.length, ei) : str.substring(si, ei + end.length)
+}
+
+const fetchData = async () => {
+	return new Promise(resolve => {
+		https.get(`https://evt05.tiancity.com/luoqi/51587/home/index.php`, res => {
+			res.setEncoding('utf8');
+			let rawData = '';
+			res.on('data', chunk => {
+				rawData += chunk
+			});
+			res.on('end', () => {
+				resolve(rawData)
+			})
+			res.on('error', e => {
+				console.log(`===== FETCH MABINOGI CARROT DATA ERROR =====`)
+				console.log(e.message)
+				resolve('')
+			})
+		})
+	})
+}
+carrot()
+
+// module.exports = {
+// 	carrot
+// }
