117940773d17d6e0752e4ace631da8439e73a132
Flandre <flantan233@gmail.com>
2022-08-04 16:03:57 +0800

    update theme

--- ai/mabinogi/ferryTimetable.js
+++ ai/mabinogi/ferryTimetable.js
@@ -34,46 +34,142 @@ const calcOffset = (time1, time2, intervalArray) => {
 	return (time2.getTime() - time1.getTime() - ~~((time2.getTime() - time1.getTime()) / basePeriod) * basePeriod) / 1000
 }
 
-const ChannelOffset = {
-	// 'Eavan': [7, -62, -1, -13, 11, 8, -13, -5, 25, 0],
-	// 'Eavan': [-25, -70, -11, -16, 5, -2, -16, -19, 299, 0],
-	'Eavan': [-6, -77, -12, -21, 14, -6, -15, -14, 7, 0],
-	// 'Altam': [31, 43, -100, -121, 254, -117, 239, -215, -133, -116],
-	'Altam': [-274, -291, -283, 228, 211, 217, 200, 158, 221, 226],
-	'Pihne': [167, 189, 190, 186, 322, 207, 175],
-}
+// const ChannelOffset = {
+// 	// 'Eavan': [7, -62, -1, -13, 11, 8, -13, -5, 25, 0],
+// 	// 'Eavan': [-25, -70, -11, -16, 5, -2, -16, -19, 299, 0],
+// 	'Eavan': [-6, -77, -12, -21, 14, -6, -15, -14, 7, 0],
+// 	// 'Altam': [31, 43, -100, -121, 254, -117, 239, -215, -133, -116],
+// 	'Altam': [-274, -291, -283, 228, 211, 217, 200, 158, 221, 226],
+// 	'Pihne': [167, 189, 190, 186, 322, 207, 175],
+// }
+//
+// const BaseTime = {
+// 	Iria: {
+// 		baseStr: '2022-07-27 18:09:12',
+// 		// 卡普港要+60
+// 		// base: new Date('2022-06-24 1:55:28'), //到港时间
+// 		// base: new Date('2022-07-18 18:07:53'), //到港时间
+// 		// base: new Date('2022-07-20 18:08:33'), //到港时间 -1：20
+// 		// base: new Date('2022-07-27 18:09:12'), //到港时间 +279 / +4'39"
+// 		base: new Date('2022-08-04 01:39:32'), //到港时间 +200 / +3'20"
+// 		interval: [5*60*1000, 6*60*1000, 4*60*1000], //等待到港时间，等待开船时间，等待到目的地时间 //Eavan Pihne Altam
+// 	},
+// 	// Connous: {
+// 	// 	base: new Date('2022-07-18 18:07:53'), //到港时间
+// 	// 	interval: [5*60*1000, 6*60*1000, 4*60*1000] //等待到港时间，等待开船时间，等待到目的地时间
+// 	// },
+// 	// Qilla: {
+// 	// 	base: new Date('2022-07-18 20:18:23'), //到港时间
+// 	// 	interval: [5*60*1000, 6*60*1000, 4*60*1000] //等待到港时间，等待开船时间，等待到目的地时间
+// 	// },
+// 	Belvast: {
+// 		baseStr: '2022-07-27 18:24:42',
+// 		// 卡普港要+30
+// 		// base: new Date('2022-06-24 2:58:28'), //到港时间
+// 		// base: new Date('2022-07-18 d17:00:27'), //到港时间
+// 		// base: new Date('2022-07-22 2:22:05'), //到港时间 - 2：20
+// 		// base: new Date('2022-07-27 18:24:42'), //到港时间 +37 / +0'37"
+// 		base: new Date('2022-08-04 01:36:02'), //到港时间 +20 / +0'20"
+// 		// base: new Date('2022-08-04 03:14:19'), //到港时间 +20 / +0'20" Altam 10
+// 		// base: new Date('2022-08-04 03:07:35'), //到港时间 +20 / +0'20" Pihne 7
+//
+// 		interval: [(2*60+30)*1000, (3*60+30)*1000, 2*60*1000], //等待到港时间，等待开船时间，等待到目的地时间
+// 	}
+// }
+//
+// const Ferry = [
+// 	{
+// 		from: 'Port Sella',
+// 		to: 'Port Ceann',
+// 		baseTime: 'Iria',
+// 		timeOffset: 0,
+// 	},
+// 	{
+// 		from: 'Port Connous',
+// 		to: 'Port Ceann',
+// 		baseTime: 'Iria',
+// 		timeOffset: 0,
+// 	},
+// 	{
+// 		from: 'Port Ceann',
+// 		to: 'Port Sella',
+// 		baseTime: 'Iria',
+// 		timeOffset: -60,
+// 	},
+// 	{
+// 		from: 'Port Ceann',
+// 		to: 'Port Connous',
+// 		baseTime: 'Iria',
+// 		timeOffset: -30,
+// 	},
+// 	{
+// 		from: 'Port Cobh',
+// 		to: 'Port Qilla',
+// 		baseTime: 'Iria',
+// 		timeOffset: -60,
+// 		checkTicket: true
+// 	},
+// 	{
+// 		from: 'Port Qilla',
+// 		to: 'Port Cobh',
+// 		baseTime: 'Iria',
+// 		timeOffset: -30,
+// 	},
+// 	{
+// 		from: 'Port Cobh',
+// 		to: 'Belvast Island',
+// 		baseTime: 'Belvast',
+// 		timeOffset: -30,
+// 		checkTicket: true
+// 	},
+// 	{
+// 		from: 'Belvast Island',
+// 		to: 'Port Cobh',
+// 		baseTime: 'Belvast',
+// 		timeOffset: 0,
+// 	}
+// ]
+
 
 const BaseTime = {
-	Iria: {
-		baseStr: '2022-07-27 18:09:12',
-		// 卡普港要+60
-		// base: new Date('2022-06-24 1:55:28'), //到港时间
-		// base: new Date('2022-07-18 18:07:53'), //到港时间
-		// base: new Date('2022-07-20 18:08:33'), //到港时间 -1：20
-		// base: new Date('2022-07-27 18:09:12'), //到港时间 +279 / +4'39"
-		base: new Date('2022-08-04 01:39:32'), //到港时间 +200 / +3'20"
-		interval: [5*60*1000, 6*60*1000, 4*60*1000], //等待到港时间，等待开船时间，等待到目的地时间 //Eavan Pihne Altam
+	'Eavan': {
+		base: {
+			Iria: {
+				timeStr: '2022-08-04 01:39:32',
+				interval: [5*60*1000, 6*60*1000, 4*60*1000],
+			},
+			Belvast: {
+				timeStr: '2022-08-04 01:36:02',
+				interval: [(2*60+30)*1000, (3*60+30)*1000, 2*60*1000]
+			}
+		},
+		channelOffset: [-6, -77, -12, -21, 14, -6, -15, -14, 7, 0]
 	},
-	// Connous: {
-	// 	base: new Date('2022-07-18 18:07:53'), //到港时间
-	// 	interval: [5*60*1000, 6*60*1000, 4*60*1000] //等待到港时间，等待开船时间，等待到目的地时间
-	// },
-	// Qilla: {
-	// 	base: new Date('2022-07-18 20:18:23'), //到港时间
-	// 	interval: [5*60*1000, 6*60*1000, 4*60*1000] //等待到港时间，等待开船时间，等待到目的地时间
-	// },
-	Belvast: {
-		baseStr: '2022-07-27 18:24:42',
-		// 卡普港要+30
-		// base: new Date('2022-06-24 2:58:28'), //到港时间
-		// base: new Date('2022-07-18 d17:00:27'), //到港时间
-		// base: new Date('2022-07-22 2:22:05'), //到港时间 - 2：20
-		// base: new Date('2022-07-27 18:24:42'), //到港时间 +37 / +0'37"
-		base: new Date('2022-08-04 01:36:02'), //到港时间 +20 / +0'20"
-		// base: new Date('2022-08-04 03:14:19'), //到港时间 +20 / +0'20" Altam 10
-		// base: new Date('2022-08-04 03:07:35'), //到港时间 +20 / +0'20" Pihne 7
-
-		interval: [(2*60+30)*1000, (3*60+30)*1000, 2*60*1000], //等待到港时间，等待开船时间，等待到目的地时间
+	'Altam': {
+		base: {
+			Iria: {
+				timeStr: '2022-08-04 01:43:18',
+				interval: [5*60*1000, 6*60*1000, 4*60*1000]
+			},
+			Belvast: {
+				timeStr: '2022-08-04 03:14:19',
+				interval: [(2*60+30)*1000, (3*60+30)*1000, 2*60*1000],
+			}
+		},
+		channelOffset: [160, 143, 151, 2, -15, -9, -26, -68, -5, 0]
+	},
+	'Pihne': {
+		base: {
+			Iria: {
+				timeStr: '2022-08-04 01:42:27',
+				interval: [5*60*1000, 6*60*1000, 4*60*1000],
+			},
+			Belvast: {
+				timeStr: '2022-08-04 03:07:35',
+				interval: [(2*60+30)*1000, (3*60+30)*1000, 2*60*1000]
+			}
+		},
+		channelOffset: [8, 14, 15, 11, 147, 32, 0]
 	}
 }
 
@@ -131,8 +227,9 @@ const Ferry = [
 ]
 
 const FindCurrentTimes = (now, baseTimeId, timeOffset, server = 'Eavan', channel = 9, count = 1) => {
-	let { base, interval } = BaseTime[baseTimeId]
-	let baseTime = base.getTime() - interval[0] + (ChannelOffset[server][channel] || 0) * 1000
+	let { timeStr, interval } = BaseTime[server].base[baseTimeId]
+	let base = new Date(timeStr)
+	let baseTime = base.getTime() - interval[0] + (BaseTime[server].channelOffset[channel] || 0) * 1000
 	let waitTime = baseTime + ~~((now - (baseTime + timeOffset * 1000)) / (interval[0] + interval[1])) * (interval[0] + interval[1]) + timeOffset * 1000
 	let out = [], tmpTime
 	for(let i = 0; i < count; i ++) {
@@ -250,29 +347,29 @@ const RenderFerryImage = (now, info, ChannelOffset, callback) => {
     	.main-container .info-group{
     		color: ${THEMES.TEXT};
     		margin-bottom: 15px;
-    		font-family: SANS_SERIF;
     	}
     	.main-container .info-group .label{
+    		font-family: AGT_SUPER_BOLD;
     		font-size: 16px;
     	}
-    	.main-container .info-group .desc{
+    	.main-container .info-group .desc-line{
     		font-size: 16px;
     		margin-top: 5px;
     	}
-    	.main-container .info-group .desc .desc-line{
-    		margin-top: 3px;
+    	.main-container .info-group .desc-line span{
+    		margin-right: 10px;
     	}
-    	.main-container .info-group .desc.offset-status{
+    	.main-container .info-group .desc-line .desc-label{
+    		margin-right: 20px;
+    	}
+    	.main-container .info-group .desc-line .offset-status{
     		font-family: MalbergTrial;
     	}
-    	.main-container .info-group .desc.offset-status .early{
+    	.main-container .info-group .desc-line .offset-status.early{
     		color: #34ff34
     	}
-    	.main-container .info-group .desc.offset-status .late{
+    	.main-container .info-group .desc-line .offset-status.late{
     		color: #d80000
-    	}
-    	.main-container .info-group .desc span{
-    		margin-right: 10px;
     	}
     	.port-group {
     		margin-bottom: 20px;
@@ -350,27 +447,25 @@ const RenderFerryImage = (now, info, ChannelOffset, callback) => {
   </head>
   <body>
   	<div class="main-container">
-  		<div class="info-group">
-  			<div class="label">Update Time</div>
-  			<div class="desc">
-					${Object.keys(BaseTime).map(area => `<span>${area}: ${BaseTime[area].baseStr}</span>`).join('')}
+  		${Object.keys(BaseTime).map(server => `
+  			<div class="info-group">
+					<div class="label">${server}</div>
+					<div class="desc-line">
+						<span class="desc-label">Update Time</span>
+						${Object.keys(BaseTime[server].base).map(area => `<span>${area}: ${BaseTime[server].base[area].timeStr}</span>`).join('')}
+					</div>
+					<div class="desc-line">
+						<span class="desc-label">Channel Offset</span>
+						${ChannelOffset[server].map(offset => `
+							<span 
+								class="offset-status ${(offset == 0 || offset == null) ? '' : offset < 0 ? 'early' : 'late'}"
+							>
+								${(offset == 0 || offset == null) ? '' : offset < 0 ? '-' : '+'}
+								${offset == null ? '-:--' : RenderCountDown(offset < 0 ? -offset * 1000 : offset * 1000)}
+							</span>`).join('')}
+					</div>
 				</div>
-			</div>
-  		<div class="info-group">
-  			<div class="label">Channel Offset</div>
-  			<div class="desc offset-status">
-					${
-						Object.keys(ChannelOffset).map(server =>
-							`<div class="desc-line">
-								<span>${server}</span>
-								${ChannelOffset[server].map(offset =>
-									`<span class="${offset == 0 ? '' : offset < 0 ? 'early' : 'late'}">${offset == 0 ? '' : offset < 0 ? '-' : '+'}${RenderCountDown(offset < 0 ? -offset * 1000 : offset * 1000)}</span>`
-								).join('')}
-							</div>`
-						).join('')
-					}
-				</div>
-			</div>
+  		`)}
   		${info.map(port => `
 				<div class="port-group">
 					<div class="port-label">${port.label}</div>
@@ -389,9 +484,10 @@ const RenderFerryImage = (now, info, ChannelOffset, callback) => {
 								<div class="time-table-col time-table-col-4">
 									${arrival.status.group == index ? `
 										${[
-			'<span class="status wait">WAIT</span>',
-			'<span class="status check-in">CHECK IN</span>'
-		][arrival.status.target]}(${RenderCountDown(arrival.status.timeOffset)})
+												'<span class="status wait">WAIT</span>',
+												'<span class="status check-in">CHECK IN</span>'
+											][arrival.status.target]
+										}(${RenderCountDown(arrival.status.timeOffset)})
 									` : ''}
 								</div>
 							</div>
@@ -420,12 +516,15 @@ const FerryTimetable = (content, qq, groupId, callback) => {
 	switch(content.substring(0, 2).toUpperCase()) {
 		case 'YW':
 			server = 'Eavan'
+			channel = 9
 			break
 		case 'YT':
 			server = 'Altam'
+			channel = 9
 			break
 		case 'PN':
 			server = 'Pihne'
+			channel = 6
 			break
 	}
 	let ct = content.substring(2, 4)
@@ -436,6 +535,9 @@ const FerryTimetable = (content, qq, groupId, callback) => {
 		if(/\d/.test(ct) || ct > 0) {
 			channel = ct - 1
 		}
+	}
+	if(server == 'Pihne' && channel > 6) {
+		server = 6
 	}
 	let now = Date.now()
 	let info = Array.from(new Set(Ferry.map(x => x.from))).map(port => {
@@ -461,9 +563,13 @@ const FerryTimetable = (content, qq, groupId, callback) => {
 	// })
 	// console.log(out)
 
-	let newChannelOffset = {}, target = ChannelOffset[server][channel] || 0
-	Object.keys(ChannelOffset).forEach(sn => {
-		newChannelOffset[sn] = ChannelOffset[sn].map(x => x - target)
+	let newChannelOffset = {}, target = BaseTime[server].channelOffset[channel] || 0
+	Object.keys(BaseTime).forEach(sn => {
+		if(sn == server) {
+			newChannelOffset[sn] = BaseTime[sn].channelOffset.map(x => x - target)
+		} else {
+			newChannelOffset[sn] = BaseTime[sn].channelOffset.map(x => null)
+		}
 	})
 
 	RenderFerryImage(now, info, newChannelOffset, callback)
