a8362f99d7d6fb99b220c2bbe3ec9e5499241397
Flandre <flantan233@gmail.com>
2022-09-08 15:25:20 +0800

    test wordcloud

--- ai/chat/personas/index.js
+++ ai/chat/personas/index.js
@@ -110,11 +110,14 @@ const renderChatPersonas = async (groupId, callback) => {
 			
 				var option = {
 					series: [{
-						type: 'wordCloud',
+						type: 'wordCloud',,
+						width: '100%',
+						height: '100%',
 						sizeRange: [4, 150],
-						rotationRange: [0, 0],
+						rotationRange: [-90, 90],
+						rotationStep: 15,
 						gridSize: 0,
-						shape: 'pentagon',
+						shape: 'circle',
 						drawOutOfBound: false,
 						keepAspect: true,
 						textStyle: {
@@ -125,9 +128,9 @@ const renderChatPersonas = async (groupId, callback) => {
 									// Math.round(Math.random() * 200) + 50,
 									// Math.round(Math.random() * 50),
 									// Math.round(Math.random() * 50) + 50
-									Math.round(Math.random() * 150) + 100,
-									Math.round(Math.random() * 150) + 100,
-									Math.round(Math.random() * 150) + 100
+									Math.round(Math.random() * 255) + 0,
+									Math.round(Math.random() * 255) + 0,
+									Math.round(Math.random() * 255) + 0
 									// 255,
 									// 255,
 									// c
