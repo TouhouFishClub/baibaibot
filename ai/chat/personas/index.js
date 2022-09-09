69796e03a785cf3aa57628acbfc79f27bcf71a10
Flandre <flantan233@gmail.com>
2022-09-09 15:37:31 +0800

    ignoer ergo module

--- ai/chat/personas/index.js
+++ ai/chat/personas/index.js
@@ -24,6 +24,10 @@ const analysisChatData = data => {
 			let filterCQ = msg.d.split('[CQ:').map((x, i) => i ? x.split(']')[1]: x).filter(x => x.trim()).join('')
 			let splitEn = Array.from(filterCQ.matchAll(/[a-zA-Z0-9]+/g)).map(x => x[0])
 
+			if(filterCQ.match(/^尔格\d{1,2}突破\d{1,5}手$/)) {
+				return
+			}
+			
 			splitEn.forEach(en => {
 				filterCQ = filterCQ.split(en).join('')
 				if(/^\d+$/.test(en)){
@@ -31,9 +35,6 @@ const analysisChatData = data => {
 				}
 				msgList.push(en)
 			})
-			if(filterCQ.match(/^尔格\d{1,2}突破\d{1,5}手$/)) {
-				return
-			}
 
 			msgList.push(filterCQ)
 		}
