2220217527926f8738aaf15cbe89951c470c16d4
Flandre <flantan233@gmail.com>
2022-09-26 16:54:42 +0800

    update push

--- ai/GenshinImpact/GenshinPush.js
+++ ai/GenshinImpact/GenshinPush.js
@@ -1,8 +1,11 @@
-
+const http = require("http");
 const path = require("path");
 const _ = require('lodash')
 const nodeHtmlToImage = require('node-html-to-image')
-const { IMAGE_DATA } = require(path.join(__dirname, '..', '..', 'baibaiConfigs.js'))
+const { IMAGE_DATA, myip } = require(path.join(__dirname, '..', '..', 'baibaiConfigs.js'))
+const font2base64 = require('node-font2base64')
+//FONTS
+const HANYIWENHEI = font2base64.encodeToDataUrlSync(path.join(__dirname, '..', '..', 'font', 'hk4e_zh-cn.ttf'))
 
 const emojiSet = new Set([
 	'🏆',
@@ -74,9 +77,9 @@ const analyzerMessage = msg => {
 			}
 		}
 	})
-	// out.users = users
+	out.users = users.filter(x => x.nickname)
 	console.log(out)
-	// this.renderImage(out)
+	renderImage(out)
 }
 
 const renderImage = data => {
@@ -87,6 +90,10 @@ const renderImage = data => {
   <meta charset="UTF-8">
   <title>Title</title>
   <style>
+		@font-face {
+			font-family: 'HANYIWENHEI';
+			src: url(${HANYIWENHEI}) format('truetype');
+		}
     * {
       border: 0;
       padding: 0;
@@ -100,27 +107,204 @@ const renderImage = data => {
       padding: 20px;
       box-sizing: border-box;
       background: #806637;
+			font-family: HANYIWENHEI;
+    }
+    .main-container {
+    	min-height: 20px;
+    }
+    .main-container .top-panel{
+    	padding: 10px 20px;
+    	background-color: #eee3cd;
+    	border-radius: 8px;
+    	display: flex;
+    	flex-direction: column;
+    	align-items: stretch;
+    	justify-content: flex-start;
+    }
+    .main-container .top-panel .title{
+    	font-size: 32px;
+    	line-height: 1;
+    	text-align: center;
+    }
+    .main-container .top-panel .check-status{
+    	margin-top: 10px;
+    	display: flex;
+    	align-items: center;
+    	justify-content: space-around;
+    }
+    .main-container .top-panel .check-status .status-item{
+    	border: 1px solid #000;
+    	font-size: 14px;
+    	padding: 0 10px;
+    	border-radius: 4px;
+    	display: flex;
+    	align-items: center;
+    	justify-content: flex-start;
+    }
+    .main-container .top-panel .check-status .status-item.success{
+    	border-color: #00BB2C;
+    	color: #00BB2C;
+    }
+    .main-container .top-panel .check-status .status-item.fail{
+    	border-color: #F44336;
+    	color: #F44336;
     }
     
+    .main-container .user-card{
+    	padding: 10px 20px;
+    	background-color: #eee3cd;
+    	display: flex;
+    	flex-direction: column;
+    	align-items: stretch;
+    	justify-content: flex-start;
+    	margin-top: 15px;
+    	border-radius: 8px;
+    	position: relative;
+    }
+    .main-container .user-card .user-top{
+    	display: flex;
+    	align-items: center;
+    	justify-content: space-between;
+    }
+    .main-container .user-card .user-top .user-info{
+    	display: flex;
+    	align-items: center;
+    	justify-content: flex-start;
+    }
+    .main-container .user-card .user-top .user-info .level{
+    	width: 40px;
+    	height: 40px;
+    	line-height: 40px;
+    	font-size: 24px;
+    	margin-right: 10px;
+    	text-align: center;
+    	border: 4px solid #000;
+    	border-radius: 50px;
+    	background-color: #fff;
+    }
+    .main-container .user-card .user-top .user-info .nickname{
+    	font-size: 32px;
+    }
+    .main-container .user-card .user-top .user-region{
+    	font-size: 20px;
+    }
+    .main-container .user-card .info-line{
+    	font-size: 20px;
+    	line-height: 26px;
+    	margin-top: 10px;
+    }
   </style>
 </head>
 <body>
+<div class="main-container">
+	<div class="top-panel">
+		<div class="title">Helper</div>
+		<div class="check-status">
+			<div class="status-item success">
+				<div class="status-label">
+					成功
+				</div>
+				<div class="status-count">
+					${ data.successCount }
+				</div>
+			</div>
+			<div class="status-item fail">
+				<div class="status-label">
+					失败
+				</div>
+				<div class="status-count">
+					${ data.failCount }
+				</div>
+			</div>
+		</div>
+	</div>
+	${data.users.map(user => `
+		<div class="user-card">
+			<div class="user-top">
+				<div class="user-info">
+					<div class="level">${user.level}</div>
+					<div class="nickname">${user.nickname}</div>
+				</div>
+				<div class="user-region">
+					${user.region_name}
+				</div>
+			</div>
+			<div class="info-line">签到状态: ${user.status}</div>
+			<div class="info-line">今天获得: ${user.reward_name}×${user.reward_cnt}</div>
+			<div class="info-line">(本月签到${user.total_sign_day}天)</div>
+			<div class="info-line">旅行者 ${new Date().getMonth() + 1 } 月札记</div>
+			<div class="info-line">原石: ${user.current_primogems}</div>
+			<div class="info-line">摩拉: ${user.current_mora}</div>
+		</div>
+	`).join('')}
+</div>
   
 </body>
 </html>`
 	let output = path.join(IMAGE_DATA, 'other', `genshin_push.png`)
-	// let output = './trade.png'
+	// let output = './genshin_push.png'
 	nodeHtmlToImage({
 		output,
 		html
 	})
 		.then(() => {
-			console.log(`保存trade.png成功！`)
+			console.log(`保存genshin_push.png成功！`)
 			let imgMsg = `[CQ:image,file=${path.join('send', 'other', `genshin_push.png`)}]`
-			callback(imgMsg)
+			sendMsg(imgMsg, {
+				port: 24334,
+				id: 577587780
+			})
 		})
 
 }
+
+const sendMsg = (msg, group) => {
+	let options = {
+		host: myip,
+		port: group.port,
+		path: `/send_group_msg?group_id=${group.id}&message=${encodeURIComponent(`${msg}`)}`,
+		method: 'GET',
+		headers: {}
+	}
+	let req = http.request(options);
+	req.on('error', function(err) {
+		console.log('req err:');
+		console.log(err);
+	});
+	req.end();
+}
+
+// (() => {
+// 	let msg = 'Genshin Impact Helper ✔ 2 · ✖ 0\n' +
+// 		'\n' +
+// 		'🏆 原神签到福利\n' +
+// 		'☁️ ✔ 2 · ✖ 0\n' +
+// 		'🌈 No.1:\n' +
+// 		'    \n' +
+// 		'    ####2022-09-26####\n' +
+// 		'    🔅芙兰 60 天空岛\n' +
+// 		"    Today's reward: 冒险家的经验 × 3\n" +
+// 		'    Total monthly check-ins: 20 days\n' +
+// 		'    Status: 👀 You have already checked-in\n' +
+// 		'    Traveler month 9 diary\n' +
+// 		'    💠primogems: 5704\n' +
+// 		'    🌕mora: 6812018\n' +
+// 		'    ##################\n' +
+// 		'🌈 No.2:\n' +
+// 		'    \n' +
+// 		'    ####2022-09-26####\n' +
+// 		'    🔅雪咪 60 天空岛\n' +
+// 		"    Today's reward: 摩拉 × 8000\n" +
+// 		'    Total monthly check-ins: 14 days\n' +
+// 		'    Status: 👀 You have already checked-in\n' +
+// 		'    Traveler month 9 diary\n' +
+// 		'    💠primogems: 5997\n' +
+// 		'    🌕mora: 7176798\n' +
+// 		'    ##################\n'
+//
+// 	analyzerMessage(msg)
+//
+// })()
 
 module.exports = {
 	analyzerMessage
