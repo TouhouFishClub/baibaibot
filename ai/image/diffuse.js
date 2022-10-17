bac576585a7da612d21228e34de23baba2b85581
ter <a26214311@gmail.com>
2022-10-17 15:15:14 +0800

    novel

--- ai/image/diffuse.js
+++ ai/image/diffuse.js
@@ -150,6 +150,7 @@ function naifu(callback,content,novelaitoken){
   var seed = Math.floor(Math.random()*4294967295)
   var bd = {"prompt":"masterpiece, best quality, "+content,"width":512,"height":768,"scale":12,"sampler":"k_euler_ancestral","steps":20,"seed":seed,"n_samples":1,"ucPreset":0,"uc":"lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"}
   bd.input=bd.prompt;
+  bd.model = 'safe-diffusion';
   var now = new Date().getTime();
   var fnc = '';
   var fn = "public/png/"+seed+"_"+fnc+"_"+now;
@@ -201,6 +202,7 @@ async function novelAIDiffuse(content,gid,qq,callback){
         'content-type': 'application/json',
         'referer': 'https://novelai.net/'
       },
+      proxy: 'http://192.168.17.241:2346',
       body: JSON.stringify(bd)
     }, function (error, response, resbody) {
       if (error && error.code) {
