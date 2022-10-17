f202bfc61afcc0b4f1d3abc8b28c9856d9719ec7
ter <a26214311@gmail.com>
2022-10-17 09:56:22 +0800

    dif

--- ai/image/diffuse.js
+++ ai/image/diffuse.js
@@ -143,7 +143,8 @@ function novelAI(callback,content){
 
 function naifu(callback,content){
   content=content.substring(4).trim();
-  var url = 'https://rebate-aggressive-rehab-author.trycloudflare.com/generate-stream';
+  var naifuurl = secret.u3;
+  var url = naifuurl;
   var seed = Math.floor(Math.random()*4294967295)
   var bd = {"prompt":"masterpiece, best quality, "+content,"width":512,"height":768,"scale":12,"sampler":"k_euler_ancestral","steps":20,"seed":seed,"n_samples":1,"ucPreset":0,"uc":"lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry"}
   var now = new Date().getTime();
