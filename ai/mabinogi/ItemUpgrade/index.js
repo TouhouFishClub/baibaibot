efc1755c177a42d429af9ab51aa0bb660b57960c
Flandre <flantan233@gmail.com>
2022-10-17 21:54:40 +0800

    add mabinogi item upgrade

--- ai/mabinogi/ItemUpgrade/index.js
+++ ai/mabinogi/ItemUpgrade/index.js
@@ -1,6 +1,11 @@
 const fs = require('fs-extra')
 const path = require('path')
 const xml2js = require('xml2js')
+const nodeHtmlToImage = require('node-html-to-image')
+const { IMAGE_DATA } = require(path.join(__dirname, '..', '..', '..', 'baibaiConfigs.js'))
+const font2base64 = require('node-font2base64')
+//FONTS
+const HANYIWENHEI = font2base64.encodeToDataUrlSync(path.join(__dirname, '..', '..', '..', 'font', 'hk4e_zh-cn.ttf'))
 
 const parser = new xml2js.Parser()
 let filterDataStorage = {}
@@ -42,7 +47,16 @@ const filterItem = async () => {
 		data.Items.Mabi_Item.forEach(item => {
 			if(item.$.Par_UpgradeMax && item.$.Par_UpgradeMax > 0 && (!item.$.Locale || item.$.Locale === 'china')) {
 				let localeNameCn = item.$.Text_Name1 ? txtData[index][item.$.Text_Name1] : 'NULL'
-				if(localeNameCn && localeNameCn.indexOf('临时') === -1 && localeNameCn.indexOf('不开放') === -1 && !localeNameCn.startsWith('@')) {
+				if(
+					localeNameCn &&
+					localeNameCn.indexOf('临时') === -1 &&
+					localeNameCn.indexOf('不开放') === -1 &&
+					localeNameCn.indexOf('禁止') === -1 &&
+					!localeNameCn.startsWith('@') &&
+					!localeNameCn.startsWith('精灵') &&
+					!localeNameCn.startsWith('交易') &&
+					!localeNameCn.startsWith('专用')
+				) {
 					// console.log(item.$.Par_UpgradeMax, `${txtData[index][item.$.Text_Name1]}`)
 					filterData[item.$.ID] = Object.assign(item.$, {
 						localeNameCn
@@ -64,6 +78,29 @@ const formatUpgradeInfo = async () => {
 		let sp = val.split('\t')
 		transform[`_LT[xml.itemupgradedb.${sp[0].trim()}]`] = sp[1]
 	})
+
+	// test output
+	// let output = {}
+	// xmlData.upgrade_db.upgrade.forEach(item => {
+	// 	if(item.$.effect) {
+	// 		item.$.effect.split(';').forEach(ef => {
+	// 			let efs = ef.split('(')
+	// 			if(efs.length < 2) {
+	// 				return
+	// 			}
+	// 			console.log(efs)
+	// 			if(output[efs[0]]) {
+	// 				output[efs[0]].itemSet.add(efs[1].split(',')[0].trim())
+	// 			} else {
+	// 				output[efs[0]] = {
+	// 					itemSet: new Set([efs[1].split(',')[0].trim()])
+	// 				}
+	// 			}
+	// 		})
+	// 	}
+	// })
+	// console.log(output)
+
 	return xmlData.upgrade_db.upgrade.map(item => Object.assign(item.$, {
 		localnameCn: transform[item.$.localname].trim(),
 		descCn: transform[item.$.desc].trim(),
@@ -97,15 +134,179 @@ const searchEquipUpgrade = async (content, callback) => {
 	}
 	if(filterEq.length === 1) {
 		let meu = await matchEquipUpgrade(filterEq[0].Category)
-		console.log(meu.map(x => `${x.localnameCn}: ${x.descCn}`).join('\n'))
-		console.log(meu.length)
+		renderImage(filterEq[0], meu)
 		return
 	}
 	if(filterEq.length === 0) {
 		callback(`未找到${content}`)
 		return
 	}
-	callback(filterEq.splice(0, 10).map(x => `meu ${x.ID} | ${x.localeNameCn}`).join('\n'))
+	callback(filterEq.splice(0, 100).map(x => `meu ${x.ID} | ${x.localeNameCn}`).join('\n'))
 }
 
-searchEquipUpgrade('40011', d => {console.log(d)})
+// 定义武器升级详细信息, 返回HTML
+const analyzerEffect = effectStr => {
+	if(effectStr.startsWith('modify')) {
+		let [type, info] = effectStr.substring(7, effectStr.length - 1).split(',').map(x => x.trim())
+		let typeCn = {
+			'balance': '平衡性',
+			'critical': '暴击率',
+			'attack_max': '最大攻击力',
+			'attack_min': '最小攻击力',
+			'durability_max': '最大耐久度',
+			'attack_range': '远程攻击距离',
+			'wound_max' : '最大负伤率',
+			'wound_min' : '最小负伤率',
+			'defense': '防御',
+			'protect': '保护',
+			'collecting_speed' : '提高采集速度',
+			'collecting_bonus' : '采集时有一定几率同时采集2个',
+			'splash_radius' : '范围攻击时增加攻击距离',
+			'splash_damage' : '范围攻击时增加攻击伤害值',
+			'immune_melee': '近战攻击自动防御率',
+			'immune_ranged': '远程攻击自动防御率',
+			'immune_magic': '魔法攻击自动防御率',
+			'manause_revised': '魔法消耗减少',
+			'manaburn_revised' : '',
+			'chain_casting' : '',
+			'magic_damage': '魔法攻击力',
+			'casting_speed': 'xx速度',
+			'lance_piercing': '穿刺等级',
+			'musicbuff_bonus' : '',
+			'musicbuff_duration' : '',
+			'max_bullet': '最大装弹数增加',
+			'magic_defense' : '魔法防御',
+			'magic_protect': '魔法保护'
+		}[type] || type
+		return `<span style="color: ${info > 0 ? '#1083FF' : '#FB0007'}">${typeCn} ${info > 0 ? '+' : ''}${info}</span>`
+	}
+	return effectStr
+}
+
+const renderImage = (targetItem, upgradeInfos) => {
+	console.log(upgradeInfos.map(x => `[${x.id}]（${x.upgraded_min} ~ ${x.upgraded_max}）${x.localnameCn}: ${x.descCn}`).join('\n'))
+	console.log(upgradeInfos.length)
+	let normalUpgrade = [], gemUpgrade = []
+	upgradeInfos.forEach(item => {
+		if(item.hasOwnProperty('upgraded_max') && item.hasOwnProperty('upgraded_min')) {
+			normalUpgrade.push(item)
+		} else {
+			gemUpgrade.push(item)
+		}
+	})
+	normalUpgrade.sort((a, b) => a.upgraded_max - b.upgraded_max).sort((a, b) => a.upgraded_min - b.upgraded_min)
+	let html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+  <style>
+		@font-face {
+			font-family: 'HANYIWENHEI';
+			src: url(${HANYIWENHEI}) format('truetype');
+		}
+    * {
+      border: 0;
+      padding: 0;
+      margin: 0;
+      font-size: 14px;
+      line-height: 1.4;
+    }
+    body {
+      width: 800px;
+      min-height: 20px;
+      padding: 20px;
+      box-sizing: border-box;
+      background: #806637;
+			font-family: HANYIWENHEI;
+    }
+    .main-container {
+    	min-height: 20px;
+    }
+    .main-container .top-panel{
+    	padding: 10px 20px;
+    	background-color: #eee3cd;
+    	border-radius: 8px;
+    	display: flex;
+    	flex-direction: column;
+    	align-items: stretch;
+    	justify-content: flex-start;
+    }
+    .main-container .top-panel .title{
+    	font-size: 32px;
+    	line-height: 1;
+    	text-align: center;
+    }
+    .main-container .top-panel .date{
+    	font-size: 14px;
+    	margin-top: 10px;
+    	line-height: 1;
+    	text-align: center;
+    }
+    
+    .main-container .upgrade-card{
+    	padding: 10px 20px;
+    	background-color: #eee3cd;
+    	display: flex;
+    	flex-direction: column;
+    	align-items: stretch;
+    	justify-content: flex-start;
+    	margin-top: 15px;
+    	border-radius: 8px;
+    	position: relative;
+    }
+    
+    .main-container .upgrade-card .upgrade-item{
+    	font-size: 14px;
+    	line-height: 20px;
+    }
+    
+    .main-container .upgrade-card .upgrade-item + .upgrade-item{
+    	border-top: 1px dashed #000;
+    }
+  </style>
+</head>
+<body>
+<div class="main-container">
+	<div class="top-panel">
+		<div class="title">${targetItem.localeNameCn}</div>
+		<div class="date">改造数据</div>
+	</div>
+	<div class="upgrade-card">
+		${normalUpgrade.map(x => `
+			<div class="upgrade-item">
+				（${x.upgraded_min} ~ ${x.upgraded_max}）${x.localnameCn}: ${x.descCn}
+				<br/>
+				效果: ${x.effect.split(';').map(ef => analyzerEffect(ef)).join(' ')}
+				<br/>
+				改造NPC: ${x.available_npc}
+			</div>
+		`).join('')}
+	</div>
+	<div class="upgrade-card">
+		${gemUpgrade.map(x => `<div class="upgrade-item">（0 ~ 0）${x.localnameCn}: ${x.descCn}</div>`).join('')}
+	</div>
+</div>
+  
+</body>
+</html>`
+	// let output = path.join(IMAGE_DATA, 'other', `MabiItemUpgrade.png`)
+	let output = './MabiItemUpgrade.png'
+	nodeHtmlToImage({
+		output,
+		html
+	})
+		.then(() => {
+			console.log(`保存MabiItemUpgrade.png成功！`)
+		})
+
+}
+
+//
+// searchEquipUpgrade('41440', d => {console.log(d)})
+// 这是无尽绝望巨锤
+// searchEquipUpgrade('41440', d => {console.log(d)})
+// 这是采集用小刀
+// searchEquipUpgrade('40023', d => {console.log(d)})
+searchEquipUpgrade('30412', d => {console.log(d)})
