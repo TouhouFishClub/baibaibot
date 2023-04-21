e3f8afcb44bbc107564524dcc840c7a4ff894322
时空.blue <a26214311@gmail.com>
2023-04-21 19:30:14 +0800

    Update baibai2.js

--- baibai2.js
+++ baibai2.js
@@ -796,7 +796,7 @@ function handle_msg_D2(content,from,name,groupid,callback,groupName,nickname,msg
     handleCustomChatgptReplay(content,groupid,from,callback);
     return;
   }
-  if(content.startsWith('百百 ')){
+  if(content.indexOf('百百')>=0){
     content=content.trim();
     getBaibaiReplay(content,groupid,from,callback);
     return;
