015b99824e022540c21fe34a4da371230ff7f660
Flandre <flantan233@gmail.com>
2023-05-16 16:39:03 +0800

    fix bug

--- ai/tapfishNew.js
+++ ai/tapfishNew.js
@@ -9,7 +9,7 @@ const targetPath = path.join(__dirname, '..', '../coolq-data/cq/data/image/send/
 
 const fetchImage = (url, filename) => new Promise((resolve, reject) => {
 	const reqs = request({
-		url: `http://api.vvhan.com/api/moyu`,
+		url,
 		method: "GET",
 		headers:{
 			'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
