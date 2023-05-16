8bf0c0eb4a75a2a5b1a8a7a4d9c75e15dc8fbfd7
Flandre <flantan233@gmail.com>
2023-05-16 16:26:55 +0800

    test

--- ai/tapfishNew.js
+++ ai/tapfishNew.js
@@ -1,13 +1,15 @@
 const fs = require('fs-extra')
 const path = require("path-extra")
+const util = require('node:util')
 const request = require('request')
+const requestPromise = util.promisify(request)
 
 let expire = 0
 const targetPath = path.join(__dirname, '..', '../coolq-data/cq/data/image/send/tapFish/')
 
-const fetchImage = filename => new Promise((resolve, reject) => {
+const fetchImage = (url, filename) => new Promise((resolve, reject) => {
 	const reqs = request({
-		url: `https://api.vvhan.com/api/moyu`,
+		url: `http://api.vvhan.com/api/moyu`,
 		method: "GET",
 		headers:{
 			'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
@@ -23,6 +25,13 @@ const fetchImage = filename => new Promise((resolve, reject) => {
 	})
 })
 
+const vvhanApi = async filename => await fetchImage(`http://api.vvhan.com/api/moyu`, filename)
+// https://j4u.ink/moyuya
+const tuotuodeApi = async filename => {
+	let res = await requestPromise(`https://j4u.ink/moyuya`)
+	console.log(`\n\n\n\n\n=== tuotuodeApi ===`)
+	console.log(res.body)
+}
 
 const tapFish = async callback => {
 	let d = new Date(), filename = `${d.getFullYear()}_${d.getMonth() + 1}_${d.getDate()}.png`
@@ -32,7 +41,7 @@ const tapFish = async callback => {
 	} else {
 		console.log('=============== 未下载过文件，先启用下载')
 		try {
-			await fetchImage(filename)
+			await tuotuodeApi(filename)
 		} catch (e) {
 			console.log(e)
 			return
