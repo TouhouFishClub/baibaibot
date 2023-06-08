96b5505f82da0df3c306c281e03728c9562be0b8
Flandre <flantan233@gmail.com>
2023-06-08 11:31:24 +0800

    update limit

--- baibai2.js
+++ baibai2.js
@@ -611,7 +611,7 @@ function handleMsg_D(msgObj,port, configs) {
 	callback = function (res, blank) {
 		if (res.trim().length > 0) {
 			// 添加自定义后缀
-			if(new Set([30004,30014,30024]).has(port)) {
+			if(new Set([30014,30024]).has(port)) {
 				if(Math.random() < 0.5) {
 					res = `${rdmPerpend[~~(rdmPerpend.length * Math.random())]} ${res}`
 				} else {
@@ -632,8 +632,8 @@ function handleMsg_D(msgObj,port, configs) {
 			groupExpire.set(msgObj.group_id, Date.now() + (~~(15*Math.random()))*1000)
 			break
 		case 30004:
-			// 20 + 10分钟随机
-			groupExpire.set(msgObj.group_id, Date.now() + (20*60+~~(10*60*Math.random()))*1000)
+			// 15秒随机
+			groupExpire.set(msgObj.group_id, Date.now() + (~~(15*Math.random()))*1000)
 			break
 		case 30014:
 			// 5 + 3分钟随机
