f42cd24711691131cf052cb474615d729b9a180d
ter <a26214311@gmail.com>
2023-06-21 13:30:05 +0800

    v

--- ai/voice/ysvoice.js
+++ ai/voice/ysvoice.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process')
 var request = require('request');
 var fs = require('fs')
-const wav  = require('node-wav')
 
 const genshinSpeakers = ['派蒙', '凯亚', '安柏', '丽莎', '琴', '香菱', '枫原万叶', '迪卢克', '温迪', '可莉', '早柚', '托马', '芭芭拉', '优菈', '云堇', '钟离', '魈', '凝光', '雷电将军', '北斗', '甘雨', '七七', '刻晴', '神里绫华', '戴因斯雷布', '雷泽', '神里绫人', '罗莎莉亚', '阿贝多', '八重神子', '宵宫', '荒泷一斗', '九条裟罗', '夜兰', '珊瑚宫心海', '五郎', '散兵', '女士', '达达利亚', '莫娜', '班尼特', '申鹤', '行秋', '烟绯', '久岐忍', '辛焱', '砂糖', '胡桃', '重云', '菲谢尔', '诺艾尔', '迪奥娜', '鹿野院平藏']
 
@@ -19,6 +18,31 @@ const noisew = 0.8
 const length = 1.2
 var gpu = 0;
 var ex_wav = 0;
+
+const wavHeader = [
+  'RIFF', // chunk id
+  0x24, // chunk size
+  'WAVE', // format
+  'fmt ', // subchunk 1 id
+  0x10, // subchunk 1 size
+  0x01, // audio format (PCM)
+  0x01, // num channels
+  0x44, 0xAC, // sample rate (44100)
+  0x88, 0x58, 0x01, // byte rate
+  0x02, // block align
+  0x10, // bits per sample
+  'data', // subchunk 2 id
+  0x10, // subchunk 2 size
+];
+let headerBuffer = Buffer.alloc(44);
+for (let i = 0; i < wavHeader.length; i++) {
+  if (typeof wavHeader[i] === 'string') {
+    headerBuffer.write(wavHeader[i], i * 2, 'utf16le');
+  } else {
+    headerBuffer.writeInt16LE(wavHeader[i], i * 2);
+  }
+}
+
 
 function ysVoiceReply(content,gid,qq,callback) {
   if (!(qq + "").startsWith("35747") && !(qq + "").startsWith("79901") && !(gid + "").startsWith("20570")) {
@@ -40,14 +64,16 @@ function ysVoiceReply(content,gid,qq,callback) {
     }else{
       var b64=body;
       var binary = Buffer.from(b64, 'base64')
-      const wavEncoder = new wav.WavEncoder(2, 44100, {bitDepth: 16})
-      const wavBuffers = wavEncoder.encode([binary])
-      fs.writeFileSync('output.wav', Buffer.concat(wavBuffers))
+
+      fs.writeFile('audio.wav', Buffer.concat([headerBuffer, binary]), err => {
+        if (err) throw err;
+        console.log('WAV 文件写入成功!');
+      });
     }
   })
 }
 
-ysVoiceReply('生命之色涡旋流转，七重之门现于世间，力量之塔君临九天',357474,357474,function(){})
+//ysVoiceReply('生命之色涡旋流转，七重之门现于世间，力量之塔君临九天',357474,357474,function(){})
 
 
 
