7e9dd2c989def70200a3f633c1229aa513c2813a
ter <a26214311@gmail.com>
2023-08-04 12:25:26 +0800

    a

--- baibai2.js
+++ baibai2.js
@@ -347,7 +347,7 @@ async function addSendQueue(groupid,msg,port,from){
 
 	var bdy = {"group_id": groupid, message: msg};
 	console.log("send:"+groupid+":"+msgSource);
-	if(port==25334){
+	if(port==25334&&(!groupid.startsWith("20570"))&&(!from.startsWith("35747"))){
       var bdy2 = {"user_id": from, message: msg};
       request({
         headers:{
