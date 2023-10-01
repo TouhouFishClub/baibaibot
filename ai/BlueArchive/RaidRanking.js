95be1a13759f6262a29c576bc5aebe14c4543068
Flandre Scarlet <flantan233@gmail.com>
2023-10-01 17:18:00 +0800

    Update RaidRanking.js

--- ai/BlueArchive/RaidRanking.js
+++ ai/BlueArchive/RaidRanking.js
@@ -18,11 +18,14 @@ const fetchData = () => new Promise(resolve => {
 		method: 'GET',
 		rejectUnauthorized: false,
 		headers: {
-			'Accept':'application/json',
+			'Accept':'application/json, text/plain, */*',
+			'Accept-Encoding':'gzip, deflate',
+			'Accept-Language':'zh-CN,zh;q=0.9',
+			'Connection':'keep-alive',
 			'Host':'ba.gamerhub.cn',
 			'Referer': 'http://ba.gamerhub.cn/',
 			'Upgrade-Insecure-Requests': '1',
-			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 		},
 	}, res => {
 		let chunk = ''
