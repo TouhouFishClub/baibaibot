ad44cd6fa8720a2c4cea32fe4389eb513a363e8c
Flandre <flantan233@gmail.com>
2024-01-12 15:32:43 +0800

    change api

--- ai/tapfishNew.js
+++ ai/tapfishNew.js
@@ -51,7 +51,7 @@ const tapFish = async callback => {
 	} else {
 		console.log('=============== 未下载过文件，先启用下载')
 		try {
-			await vvhanApi(filename)
+			await tuotuodeApi(filename)
 		} catch (e) {
 			console.log(e)
 			return
