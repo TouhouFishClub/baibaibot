618a967dda150c09c68a1b3ed341fcc8ab993e03
Flandre <flantan233@gmail.com>
2024-02-07 11:00:08 +0800

    update

--- ai/mabinogi/2022_red_packet.js
+++ ai/mabinogi/2022_red_packet.js
@@ -66,6 +66,8 @@ let AUTO_SEND_GROUPS = [
 ]
 
 const mabinogi_red_packet_list = callback => {
+  console.log(AUTO_SEND_GROUPS)
+  console.log(JSON.stringify(AUTO_SEND_GROUPS))
   callback(JSON.stringify(AUTO_SEND_GROUPS))
 }
 
