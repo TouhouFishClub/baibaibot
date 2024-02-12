91b35f166c3571ef6eaa3d4b43b8fab14543a148
flandre <flantan233@gmail.com>
2024-02-12 18:52:47 +0800

    test

--- ai/mabinogi/ItemUpgrade/index.js
+++ ai/mabinogi/ItemUpgrade/index.js
@@ -131,6 +131,7 @@ const formatUpgradeInfo = async () => {
 	// console.log(output)
 	// console.log(xmlData.upgrade_db.effect)
 	let effectTmp = xmlData.upgrade_db.effect.map(x => x.$)
+	console.log(effectTmp)
 
 	return [
 		xmlData.upgrade_db.upgrade.map(item => Object.assign(item.$, {
