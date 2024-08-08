80e7c712215fde3bff15a7d335c3d67d99729369
Flandre <flantan233@gmail.com>
2024-08-08 16:24:28 +0800

    update recipe

--- ai/mabinogi/recipeNew/recipeTools/index.js
+++ ai/mabinogi/recipeNew/recipeTools/index.js
@@ -1,20 +1,70 @@
 const fs = require('fs-extra')
+const process = require('process')
 const path = require('path')
 const xml2js = require('xml2js')
 
 const parser = new xml2js.Parser()
+let itemXML = []
+let itemETCXML = []
+let itemTXT = {}
+
+let itemPlus = {}
 
 eval(fs.readFileSync(path.join(__dirname, '..', '/js/TailoringItem.js')).toString('utf-8'))
 eval(fs.readFileSync(path.join(__dirname, '..', '/js/BlacksmithItem.js')).toString('utf-8'))
+eval(fs.readFileSync(path.join(__dirname, '..', '/js/Item.js')).toString('utf-8'))
 // console.log(TailoringList)
 // console.log(BlacksmithList)
 
 const TailoringSet = new Set(TailoringList.map(x => `${x}`))
 const BlackSmithSet = new Set(BlacksmithList.map(x => `${x}`))
-let items = []
 
-const loadItems = async () => {
-  let xmlData = await readXmlParse(path.join(__dirname, 'data', `ManualForm.xml`))
+const loadItemXml = async () => {
+  if(itemXML.length) {
+    return itemXML
+  } else {
+    let xmlData = await readXmlParse(path.join(__dirname, '..', '..', 'data', 'IT', `itemdb.xml`))
+    let txt = fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'IT', `itemdb.china.txt`), 'utf-8')
+    let transform = {}
+    txt.split('\n').forEach(val => {
+      let sp = val.split('\t')
+      if(sp[0] && sp[1]) {
+        transform[`_LT[xml.itemdb.${sp[0].trim()}]`] = sp[1].trim()
+      }
+    })
+    let output = xmlData.Items.Mabi_Item.map(x => Object.assign({}, x.$, {Text_China: transform[x.$.Text_Name1]}))
+    // console.log(output)
+    itemXML = output
+    return output
+  }
+}
+const loadItemETCXml = async () => {
+  if(itemETCXML.length) {
+    return itemETCXML
+  } else {
+    let xmlData = await readXmlParse(path.join(__dirname, '..', '..', 'data', 'IT', `itemdb_etc.xml`))
+    let txt = fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'IT', `itemdb_etc.china.txt`), 'utf-8')
+    let transform = {}
+    txt.split('\n').forEach(val => {
+      let sp = val.split('\t')
+      if(sp[0] && sp[1]) {
+        transform[`_LT[xml.itemdb_etc.${sp[0].trim()}]`] = sp[1].trim()
+      }
+    })
+    let output = xmlData.Items.Mabi_Item.map(x => Object.assign({}, x.$, {Text_China: transform[x.$.Text_Name1]}))
+    // console.log(output)
+    itemETCXML = output
+    return output
+  }
+}
+
+const matchCategory = (Category, itemCategory = '') => {
+  if(!Category) {
+    return false
+  }
+  // 如果需要完全按规则匹配，这里的.*改成.+
+  let newReg = Category.replace(/\*/g, '.*')
+  return new RegExp(`^${newReg}$`, 'g').test(itemCategory)
 }
 
 const readXmlParse = filePath => new Promise((resolve, reject) => {
@@ -29,37 +79,217 @@ const readXmlParse = filePath => new Promise((resolve, reject) => {
   })
 })
 
+const analyzerItem = async str => {
+  let items = await loadItemXml()
+  let sp = str.split(';').map(x => x.split(',').map(x => x.trim()).filter(x => x)).filter(x => x.length)
+  let infos = [], output = []
+  for(let i = 0; i < sp.length; i ++) {
+    let [regexp, count] = sp[i], target = {
+      item: regexp,
+      id: 9090909
+    }
+    for(let j = 0; j < items.length; j ++) {
+      let item = items[j]
+      if(matchCategory(regexp, item.Category)) {
+        // console.log(item)
+        target = {
+          item: item.Text_China,
+          id: item.ID
+        }
+        if(!item.Text_China.includes('活动')){
+          // 不知道为什么会匹配到活动的数据，暂时这样过滤一下
+          break
+        }
+      }
+    }
+    infos.push(target, count)
+  }
+  // console.log(sp)
+  // console.log(infos)
+  output = infos.map((x, i) => {
+    if(i % 2) {
+      return parseInt(x)
+    } else {
+      try {
+        process.stdout.clearLine()
+        process.stdout.cursorTo(0)
+        // 如果有，则不做添加
+        let txt = `item has ${eval(`Item${x.id}`)}`
+        // console.log(txt)
+        process.stdout.write(txt)
+      } catch (err) {
+        // console.log(err)
+        // 如果没有，则添加额外item
+        itemPlus[`Item${x.id}`] = x.item.trim()
+      }
+      return parseInt(x.id)
+    }
+  })
+  // console.log(output)
+  return output
+}
+
+const analyzerCompleteItem = async str => {
+  // process.stdout.write('\n')
+  let items = (await loadItemXml()).concat(await loadItemETCXml())
+
+  let output = []
+  let compTypes = str.split('\n').map(x => x.trim().substring(1, x.trim().length - 1))
+  if(compTypes.length > 1) {
+    // 暂定只有一种变动属性
+    compTypes.sort((a, b) => a.length - b.length)
+    // compTypes = compTypes.map((x, i) => i ? x.split(compTypes[0]).join('|') : x)
+    output.push("Ingot", 1)
+  }
+
+  let sp = compTypes[0].split(';').map(x => x.split(',').map(x => x.trim()).filter(x => x)).filter(x => x.length)
+  let infos = []
+  for(let i = 0; i < sp.length; i ++) {
+    let [regexp, count] = sp[i], target = {
+      item: regexp,
+      id: 9090909
+    }
+    for(let j = 0; j < items.length; j ++) {
+      let item = items[j]
+      if(matchCategory(regexp, item.Category)) {
+        // console.log(item)
+        target = {
+          item: item.Text_China,
+          id: item.ID
+        }
+        if(!item.Text_China.includes('活动')){
+          // 不知道为什么会匹配到活动的数据，暂时这样过滤一下
+          break
+        }
+      }
+    }
+    infos.push(target, count)
+  }
+
+  // console.log(sp)
+  // console.log(infos)
+  // console.log(output)
+  output = output.concat(infos.map((x, i) => {
+    if(i % 2) {
+      return parseInt(x)
+    } else {
+      try {
+        process.stdout.clearLine()
+        process.stdout.cursorTo(0)
+        // 如果有，则不做添加
+        let txt = `item has ${eval(`Item${x.id}`)}`
+        // console.log(txt)
+        process.stdout.write(txt)
+      } catch (err) {
+        // console.log(err)
+        // 如果没有，则添加额外item
+        itemPlus[`Item${x.id}`] = x.item.trim()
+      }
+      return parseInt(x.id)
+    }
+  }))
+  // console.log(output)
+  return output
+}
+
+// 参考 44008 恶魔孤寂拳套 BlacksmithItem44008
 const analyzer = async () => {
   let xmlData = await readXmlParse(path.join(__dirname, 'data', `ManualForm.xml`))
+  let txt = fs.readFileSync(path.join(__dirname, 'data', `ManualForm.china.txt`), 'utf-8')
+  let transform = {}
+  txt.split('\n').forEach(val => {
+    let sp = val.split('\t')
+    transform[`_LT[xml.manualform.${sp[0].trim()}]`] = sp[1]
+  })
   let Tailoring = {}, BlackSmith = {}
   let { ManualForm } = xmlData
-  Object.keys(ManualForm).forEach(cate => {
-    ManualForm[cate][0].ManualForm.forEach(x => {
+  const ManualFormKeys = Object.keys(ManualForm)
+  for(let i = 0; i < ManualFormKeys.length; i++) {
+    let cate = ManualFormKeys[i]
+    for(let j = 0; j < ManualForm[cate][0].ManualForm.length; j++) {
+      let xmlItem = ManualForm[cate][0].ManualForm[j].$
       if(cate.startsWith('Tailoring')) {
         // 已经有的过滤，不再重新添加
-        if(!TailoringSet.has(x.$.ProductItemID)) {
-          Tailoring[x.$.ProductItemID] = Object.assign({
-            output: JSON.stringify([[parseInt(x.$.Level), parseFloat(x.$.MaxProgress)], [x.$.Essentials, 0, x.$.CompleteEssentials]])
-          }, x.$)
+        if(!TailoringSet.has(xmlItem.ProductItemID)) {
+          try {
+            // 如果有，则不做添加
+            console.log(eval(`Item${xmlItem.ManualItemID}${xmlItem.FormID}`))
+          } catch (err) {
+            // console.log(err)
+            // 如果没有，则添加额外item
+            itemPlus[`Item${xmlItem.ManualItemID}${xmlItem.FormID}`] = transform[xmlItem.ManualNameLocal].trim()
+          }
+          let essentials = await analyzerItem(xmlItem.Essentials)
+          let completeEssentials = await analyzerCompleteItem(xmlItem.CompleteEssentials)
+          Tailoring[xmlItem.ProductItemID] = Object.assign({
+            output: JSON.stringify([
+              [
+                parseInt(xmlItem.Level),
+                parseFloat(xmlItem.MaxProgress)
+              ],
+              [
+                parseInt(`${xmlItem.ManualItemID}${xmlItem.FormID}`),
+                // xmlItem.Essentials,
+                ...essentials,
+                0,
+                // xmlItem.CompleteEssentials
+                ...completeEssentials
+              ]
+            ])
+          }, xmlItem)
         }
       }
       if(cate.startsWith('BlackSmith')) {
-        if(!BlackSmithSet.has(x.$.ProductItemID)) {
-          BlackSmith[x.$.ProductItemID] = Object.assign({
-            output: JSON.stringify([[parseInt(x.$.Level), parseFloat(x.$.MaxProgress)], [x.$.Essentials, 0, x.$.CompleteEssentials]])
-          }, x.$)
+        if(!BlackSmithSet.has(xmlItem.ProductItemID)) {
+          try {
+            // 如果有，则不做添加
+            console.log(eval(`Item${xmlItem.ManualItemID}${xmlItem.FormID}`))
+          } catch (err) {
+            // console.log(err)
+            // 如果没有，则添加额外item
+            itemPlus[`Item${xmlItem.ManualItemID}${xmlItem.FormID}`] = transform[xmlItem.ManualNameLocal].trim()
+          }
+          let essentials = await analyzerItem(xmlItem.Essentials)
+          let completeEssentials = await analyzerCompleteItem(xmlItem.CompleteEssentials)
+          BlackSmith[xmlItem.ProductItemID] = Object.assign({
+            output: JSON.stringify([
+              [
+                parseInt(xmlItem.Level),
+                parseFloat(xmlItem.MaxProgress)
+              ],
+              [
+                parseInt(`${xmlItem.ManualItemID}${xmlItem.FormID}`),
+                // xmlItem.Essentials,
+                ...essentials,
+                0,
+                // xmlItem.CompleteEssentials
+                ...completeEssentials
+              ]
+            ])
+          }, xmlItem)
         }
       }
-    })
-  })
-  // console.log(Tailoring, BlackSmith)
+    }
+  }
+
+  process.stdout.write('\n')
+  console.log(Tailoring, BlackSmith)
   Object.keys(Tailoring).forEach(x => {
+    console.log(transform[Tailoring[x].ManualNameLocal])
     console.log(`${x} = ${Tailoring[x].output}\n`)
   })
   Object.keys(BlackSmith).forEach(x => {
+    console.log(transform[BlackSmith[x].ManualNameLocal])
     console.log(`${x} = ${BlackSmith[x].output}\n`)
   })
+  // console.log(fs.readFileSync(path.join(__dirname, '..', '/js/Item.js')).toString('utf-8'))
+  console.log('==', itemPlus)
   console.log(`Tailoring count: ${Object.keys(Tailoring).length}, BlackSmith count: ${Object.keys(BlackSmith).length}, `)
+
+  console.log('=== append to TailoringItem.js file ===\n')
+  let TailoringOutputStr = `TailoringList.concat([${Object.values(Tailoring).filter(x => !x.SpecialTalent).map(x=>x.ProductItemID).join(',')}]);\nTalentTailoringList.concat([${Object.values(Tailoring).filter(x => x.SpecialTalent).map(x=>x.ProductItemID).join(',')}]);\nvar ${Object.values(Tailoring).map(x => `TailoringItem${x.ProductItemID}=${x.output}`).join(',')};`
+  console.log(TailoringOutputStr)
+  console.log('\n')
 }
 
 analyzer()
