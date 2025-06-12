214f42d08bdc45cf3ba6403f119b39c45c81bbfc
a26214311 <a26214311@gmail.com>
2025-06-12 15:42:10 +0800

    a

--- ai/weather.js
+++ ai/weather.js
@@ -64,7 +64,7 @@ function getWeatherByCity(city,userId,callback){
         headers:{
           'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
           'Referer':'https://www.weather.com.cn/'
-        }
+        },
         method: "GET"
       }, function(error, response, body){
         if(error&&error.code){
